9eaa6def294d2eccca935a3eaf22b7a5dd4276da
mluena <maluenarod@gmail.com>
2023-09-22 10:59:53 +0200

    tests

--- e2e/geostories.spec.ts
+++ e2e/geostories.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test('geostories visibility', async ({ page }) => {
   await page.goto('/map');
   const geostoriesButton = page.getByTestId('geostories-button');
-
   await geostoriesButton.click();
 
   await expect(page.getByTestId('geostories-container')).toBeVisible();
