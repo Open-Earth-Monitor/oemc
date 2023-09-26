7638568bb76ec9f89566be101ae11c68114531b5
David Inga <david.inga@vizzuality.com>
2023-09-26 12:43:35 +0200

    removed not needed libs and files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
-
 /**
  * @type { import('next').NextConfig }
  */
@@ -21,34 +7,6 @@ const nextConfig = {
   // ? https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
   // output: 'standalone',
   poweredByHeader: false,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'svg-sprite-loader',
-        },
-        {
-          loader: 'svgo-loader',
-          options: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    convertColors: { shorthex: false },
-                    convertPathData: false,
-                  },
-                },
-              },
-            ],
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
--- src/app/api/layers/[layer_id]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-import { LAYERS } from 'constants/api-payloads/layers';
-
-export function GET(request, { params }: { params: { layer_id: string } }) {
-  const { layer_id: id } = params;
-  return NextResponse.json(LAYERS.find(({ layer_id }) => id === layer_id));
-}
