b20665785586af8959d633814726e11d632f811a
mluena <maluenarod@gmail.com>
2023-09-27 12:35:26 +0200

    share tool tests

--- src/components/share/index.tsx
+++ src/components/share/index.tsx
@@ -1,16 +1,19 @@
 import { FC, useMemo, useState } from 'react';
 
+import { LinkedinShareButton, TwitterShareButton } from 'react-share';
+
 import { usePathname, useSearchParams } from 'next/navigation';
 
 import { HiOutlineShare } from 'react-icons/hi';
 import { PiLinkSimpleBold } from 'react-icons/pi';
+import { RiTwitterXLine, RiLinkedinFill } from 'react-icons/ri';
 
 import { cn } from '@/lib/classnames';
 
 import { CONTROL_BUTTON_STYLES, CONTROL_ICON_STYLES } from '@/components/map/controls/constants';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
-import SocialMedia from './social-media';
+const timeResetCopyState = 1000;
 
 export const ShareControl: FC = () => {
   const pathname = usePathname();
@@ -32,7 +35,7 @@ export const ShareControl: FC = () => {
       // Reset the "copied" state after 1 second
       setTimeout(() => {
         setURLCopyState(false);
-      }, 2000);
+      }, timeResetCopyState);
     } catch (error) {
       // Handle clipboard copy error
       console.error('Error copying to clipboard:', error);
@@ -40,7 +43,7 @@ export const ShareControl: FC = () => {
   };
   return (
     <Popover>
-      <PopoverTrigger className={CONTROL_BUTTON_STYLES.default}>
+      <PopoverTrigger className={CONTROL_BUTTON_STYLES.default} data-testid="share-tool-trigger">
         <HiOutlineShare className={CONTROL_ICON_STYLES.default} />
       </PopoverTrigger>
       <PopoverContent
@@ -49,19 +52,49 @@ export const ShareControl: FC = () => {
         align="start"
         className="ml-1.5 flex h-[34px] w-fit items-center border border-brand-50 p-0.5"
       >
-        {urlCopyState && <p className="px-2 py-1.5 text-xs">Link copied to clipboard</p>}
+        {urlCopyState && (
+          <p data-testid="copy-link-success" className="px-2 py-1.5 text-xs">
+            Link copied to clipboard
+          </p>
+        )}
         {!urlCopyState && (
           <>
             <button
               type="button"
+              data-testid="copy-url-link"
               aria-label="copy url link"
               className={cn(CONTROL_BUTTON_STYLES.default, 'flex h-[28px] w-auto space-x-2 px-2')}
               onClick={() => void handleCopy()}
             >
               <PiLinkSimpleBold className={CONTROL_ICON_STYLES.default} />
-              <span className="text-xs">Copy URL link</span>
+              <span data-testid="copy-message" className="text-xs">
+                Copy URL link
+              </span>
             </button>
-            <SocialMedia url={urlCopy} />
+            <div className="flex items-center space-x-2">
+              <TwitterShareButton
+                url={urlCopy}
+                title="Open Earth Monitor Cyberinfrastructure"
+                aria-label="share twitter"
+                data-testid="share-twitter-button"
+              >
+                <div className={cn(CONTROL_BUTTON_STYLES.default, 'h-[28px] w-[28px]')}>
+                  <RiTwitterXLine className={CONTROL_ICON_STYLES.default} />
+                </div>
+              </TwitterShareButton>
+
+              <LinkedinShareButton
+                url={urlCopy}
+                title="Open Earth Monitor Cyberinfrastructure"
+                className="align-baseline"
+                aria-label="share in linkedin"
+                data-testid="share-linkedin-button"
+              >
+                <div className={cn(CONTROL_BUTTON_STYLES.default, 'h-[28px] w-[28px]')}>
+                  <RiLinkedinFill className={CONTROL_ICON_STYLES.default} />
+                </div>
+              </LinkedinShareButton>
+            </div>
           </>
         )}
       </PopoverContent>
