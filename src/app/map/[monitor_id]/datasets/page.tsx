4dbd946ac680535ecfbbfed42d672810e49321df
David Inga <david.inga@vizzuality.com>
2023-10-02 14:45:02 +0200

    geostory card styles

--- src/app/map/[monitor_id]/datasets/page.tsx
+++ src/app/map/[monitor_id]/datasets/page.tsx
@@ -1,32 +1,23 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import type { NextPage } from 'next';
 
 import { useMonitorLayers } from '@/hooks/monitors';
 
 import DatasetCard from '@/components/datasets/card';
 import Loading from '@/components/loading';
 
-const DatasetsPage = () => {
-  const pathname = usePathname();
-  const monitorId = pathname.split('/')[2];
-  const { data, isLoading, isFetched, isError } = useMonitorLayers({ monitor_id: monitorId });
+const DatasetsPage: NextPage<{ params: { monitor_id: string } }> = ({ params: { monitor_id } }) => {
+  const { data, isLoading, isFetched, isError } = useMonitorLayers({ monitor_id });
 
   return (
-    <div className="m-auto w-full">
+    <div>
       {isLoading && <Loading />}
       {isFetched && !isError && (
         <ul className="space-y-6 text-secondary-500" data-testid="datasets-list">
-          {data.map(({ layer_id, title, download_url, description, author, range }) => (
-            <li key={layer_id}>
-              <DatasetCard
-                id={layer_id}
-                title={title}
-                download_url={download_url}
-                description={description}
-                author={author}
-                range={range}
-              />
+          {data.map((dataset) => (
+            <li key={dataset.layer_id}>
+              <DatasetCard {...dataset} id={dataset.layer_id} />
             </li>
           ))}
         </ul>
