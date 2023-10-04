abbdf68cdecd3e4163b038d8bfe06a12d3d7a326
David Inga <david.inga@vizzuality.com>
2023-10-04 11:28:06 +0200

    last minute fixes

--- src/components/map/static.tsx
+++ src/components/map/static.tsx
@@ -5,6 +5,8 @@ import ReactMapGL from 'react-map-gl';
 import MapLibreGL from 'maplibre-gl';
 
 import { DEFAULT_VIEWPORT, MAP_STYLE } from './constants';
+import Controls from './controls';
+import BookmarkControl from './controls/bookmark';
 
 const MAP_ID = 'oemc-static-map';
 
@@ -16,7 +18,11 @@ const StaticMap: React.FC<{ mapId?: string }> = ({ mapId = MAP_ID }) => {
         mapLib={MapLibreGL}
         initialViewState={DEFAULT_VIEWPORT}
         mapStyle={MAP_STYLE}
-      />
+      >
+        <Controls>
+          <BookmarkControl />
+        </Controls>
+      </ReactMapGL>
     </div>
   );
 };
