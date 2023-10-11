653d63d6e0050ed75787a4b2d8558b87d19ca72e
mluena <maluenarod@gmail.com>
2023-10-11 17:11:25 +0200

    migration to openlayers

--- src/components/map/constants.ts
+++ src/components/map/constants.ts
@@ -1,61 +1,14 @@
-import type { MapboxStyle } from 'react-map-gl';
+import { fromLonLat } from 'ol/proj';
+import { RView } from 'rlayers/RMap';
 
 import type { Bbox } from '@/components/map/types';
-
-import type { ExplicitViewState } from './types';
-
-export const MAP_STYLE = {
-  version: 8,
-  name: 'Custom',
-  metadata: {},
-  center: [0, 0],
-  zoom: 2,
-  bearing: 0,
-  pitch: 0,
-  sources: {
-    basemap: {
-      type: 'raster',
-      tiles: [
-        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      ],
-      tileSize: 256,
-      attribution:
-        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    },
-  },
-  layers: [
-    {
-      id: 'basemap',
-      source: 'basemap',
-      type: 'raster',
-      paint: {
-        'raster-contrast': 0.2,
-        'raster-saturation': -0.2,
-      },
-    },
-    {
-      id: 'custom-layers',
-      type: 'background',
-      paint: { 'background-color': 'pink', 'background-opacity': 0 },
-    },
-  ],
-} satisfies MapboxStyle;
 
 /**
  * EUROPE viewport
  */
-export const DEFAULT_VIEWPORT: ExplicitViewState = {
-  longitude: 10,
-  latitude: 48,
-  zoom: 4,
+export const DEFAULT_VIEWPORT: RView = {
+  center: fromLonLat([10, 48]),
+  zoom: 5,
 };
 
 export const DEFAULT_BBOX: Bbox = [-173.488154, -60.809359, 164.011846, 67.836775];
-
-export const DEFAULT_BOUNDS = {
-  bbox: DEFAULT_BBOX,
-  options: {
-    padding: 100,
-    duration: 1000,
-  },
-};
--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,49 +1,46 @@
 'use client';
 
-import { useEffect, useState, useCallback, FC } from 'react';
-
-import ReactMapGL, { MapProvider, ViewState, ViewStateChangeEvent } from 'react-map-gl';
+import React, { useState, FC, useCallback, useEffect, useRef } from 'react';
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-import MapLibreGL from 'maplibre-gl';
+import { MapBrowserEvent } from 'ol';
+import { RLayerWMS, RMap, RLayerTile, RControl } from 'rlayers';
+import { RView } from 'rlayers/RMap';
 
-import { DEFAULT_VIEWPORT, MAP_STYLE } from './constants';
+import { useLayerParsedSource } from '@/hooks/layers';
+import { useURLayerParams } from '@/hooks/url-params';
+
+import { DEFAULT_VIEWPORT } from './constants';
+// map controls
 import Controls from './controls';
 import BookmarkControl from './controls/bookmark';
 import ShareControl from './controls/share';
-import ZoomControl from './controls/zoom';
-import LayerManager from './layer-manager';
 import Legend from './legend';
-import type { CustomMapProps, ExplicitViewState } from './types';
-
-const CustomMap: FC<CustomMapProps> = ({
-  // * if no id is passed, react-map-gl will store the map reference in a 'default' key:
-  // * https://github.com/visgl/react-map-gl/blob/ecb27c8d02db7dd09d8104e8c2011bda6aed4b6f/src/components/use-map.tsx#L18
-  id = 'oemc-map',
-  initialViewState = DEFAULT_VIEWPORT,
-}: CustomMapProps) => {
+import type { CustomMapProps } from './types';
+const Map: FC<CustomMapProps> = ({ initialViewState = DEFAULT_VIEWPORT }) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
+  const parsedCenterParam = searchParams
+    .get('center')
+    ?.split(',')
+    .map((coord) => Number(coord));
 
   /**
    * STATE
    */
-  const [localViewState, setLocalViewState] = useState<Partial<ViewState> | ExplicitViewState>({
-    longitude: searchParams.get('longitude')
-      ? Number(searchParams.get('longitude'))
-      : initialViewState.longitude,
-    latitude: searchParams.get('latitude')
-      ? Number(searchParams.get('latitude'))
-      : initialViewState.latitude,
+  const [localViewState, setLocalViewState] = useState<RView>({
+    center: searchParams.get('center') ? parsedCenterParam : initialViewState.center,
     zoom: searchParams.get('zoom') ? Number(searchParams.get('zoom')) : initialViewState.zoom,
   });
   const [nextSearchParams, setNextSearchParams] = useState<string>(searchParams.toString());
   const [currentPathname, setCurrentPathname] = useState<string>(pathname);
 
-  const handleMapMove = useCallback<(e: ViewStateChangeEvent) => void>(({ viewState }) => {
-    setLocalViewState(viewState);
+  const handleMapMove = useCallback<
+    (e: MapBrowserEvent<UIEvent & { frameState: { viewState: RView } }>) => void
+  >((e) => {
+    setLocalViewState(e.frameState.viewState);
   }, []);
 
   /**
@@ -51,12 +48,11 @@ const CustomMap: FC<CustomMapProps> = ({
    */
   const cleanUpLayers = useCallback(() => {
     const nextSearchParams = new URLSearchParams({
-      longitude: localViewState.longitude?.toString(),
-      latitude: localViewState.latitude?.toString(),
+      center: localViewState.center?.toString(),
       zoom: localViewState.zoom?.toString(),
     });
     setNextSearchParams(nextSearchParams.toString());
-  }, [localViewState.latitude, localViewState.longitude, localViewState.zoom]);
+  }, [localViewState?.center, localViewState?.zoom]);
 
   /**
    * Update the URL when the user stops moving the map
@@ -64,8 +60,7 @@ const CustomMap: FC<CustomMapProps> = ({
   const handleUpdateUrl = useCallback(() => {
     const originalSearchParams = new URLSearchParams(searchParams.toString());
     const nextSearchParams = new URLSearchParams({
-      longitude: localViewState.longitude?.toString(),
-      latitude: localViewState.latitude?.toString(),
+      center: localViewState.center?.toString(),
       zoom: localViewState.zoom?.toString(),
     });
     originalSearchParams.forEach((value, key) => {
@@ -74,7 +69,7 @@ const CustomMap: FC<CustomMapProps> = ({
       }
     });
     setNextSearchParams(nextSearchParams.toString());
-  }, [searchParams, localViewState.longitude, localViewState.latitude, localViewState.zoom]);
+  }, [searchParams, localViewState]);
 
   /**
    * Update the viewport state when the URL pathname, and search params changes
@@ -90,30 +85,63 @@ const CustomMap: FC<CustomMapProps> = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname, nextSearchParams, router, currentPathname]);
+  const { layerId, layerOpacity, date } = useURLayerParams();
+  const { data } = useLayerParsedSource(
+    {
+      layer_id: layerId,
+    },
+    {
+      enabled: !!layerId,
+    }
+  );
 
+  const { gs_base_wms, gs_name, range } = data || {};
+  const mapRef = useRef();
   return (
-    <div className="relative z-0 h-full w-full">
-      <MapProvider>
-        <ReactMapGL
-          id={id}
-          {...localViewState}
-          mapLib={MapLibreGL}
-          onMove={handleMapMove}
-          onMoveEnd={handleUpdateUrl}
-          mapStyle={MAP_STYLE}
-          attributionControl={false}
-        >
-          <LayerManager layers={['raster']} />
-          <Controls className="absolute bottom-3 left-[554px]">
-            <ZoomControl />
-            <BookmarkControl />
-            <ShareControl />
-          </Controls>
-          <Legend />
-        </ReactMapGL>
-      </MapProvider>
-    </div>
+    <>
+      <RMap
+        width="100%"
+        height="100%"
+        className="relative"
+        initial={DEFAULT_VIEWPORT}
+        onChange={handleUpdateUrl}
+        onMoveEnd={handleMapMove}
+        view={[localViewState, handleUpdateUrl]}
+        ref={mapRef}
+        noDefaultControls
+      >
+        <RLayerTile
+          properties={{ label: 'Basemap' }}
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attributions="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+        />
+        <RLayerWMS
+          properties={{ label: gs_name, opacity: layerOpacity, date, range }}
+          url={gs_base_wms}
+          params={{
+            FORMAT: 'image/png',
+            WIDTH: 256,
+            HEIGHT: 256,
+            SERVICE: 'WMS',
+            VERSION: '1.3.0',
+            REQUEST: 'GetMap',
+            TRANSPARENT: true,
+            LAYERS: gs_name,
+            DIM_DATE: date,
+            CRS: 'EPSG:3857',
+            BBOX: 'bbox-epsg-3857',
+          }}
+        />
+
+        <Controls className="absolute bottom-3 left-[554px] z-50">
+          <RControl.RZoom />
+          <BookmarkControl />
+          <ShareControl />
+        </Controls>
+        <Legend />
+      </RMap>
+    </>
   );
 };
 
-export default CustomMap;
+export default Map;
