8108a66f4de96d3005029d0057f11df274c4c972
mluena <maluenarod@gmail.com>
2023-10-18 20:57:55 +0200

    geostories link animation

--- src/components/landing-card/geostories.tsx
+++ src/components/landing-card/geostories.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import Link from 'next/link';
 
 import { AnimatePresence, motion } from 'framer-motion';
-import { HiOutlineArrowTopRightOnSquare } from 'react-icons/hi2';
+import { HiOutlineArrowTopRightOnSquare, HiArrowRight } from 'react-icons/hi2';
 
 import cn from '@/lib/classnames';
 
@@ -37,22 +37,49 @@ const Card: FC<Partial<Geostory> & { color?: string; headColor?: string }> = ({
           href={`/map/geostories/${id}`}
           data-testid="dataset-layer-toggle-button"
           className={cn(
-            'flex items-center space-x-4 py-2 text-xs font-bold text-secondary-500 transition-colors hover:bg-secondary-500/20'
+            'flex items-center space-x-4 py-2 text-xs font-bold text-secondary-500 transition-colors hover:underline'
           )}
         >
           <HiOutlineArrowTopRightOnSquare className="h-5 w-5" />
-          <span>Go to geostory</span>
+          <span className="hover:underline ">Go to geostory</span>
         </Link>
       </div>
       <div className="p-6">
         <span className="text-xs font-medium uppercase">monitor</span>
         <ul>
           {layers.map(({ title }) => (
-            <li key={id}>
-              <Link href={`/map/${id}/datasets`} className="font-bold underline">
-                {title}
+            <motion.li key={id} className="flex items-center" initial="initial" whileHover="hover">
+              <Link
+                href={`/map/${id}/datasets`}
+                className="relative w-full items-center font-bold underline"
+              >
+                <motion.span
+                  className="absolute left-0 top-2 inline-flex h-full w-full hover:opacity-100"
+                  variants={{
+                    hover: { opacity: 1 },
+                  }}
+                  transition={{ duration: 0.15 }}
+                >
+                  <HiArrowRight className="inline-block h-5 w-5" />
+                  <motion.span
+                    className="inline-flex"
+                    variants={{
+                      initial: {
+                        x: 0,
+                        position: 'absolute',
+                        left: 0,
+                        width: '100%',
+                        backgroundColor: color,
+                      },
+                      hover: { x: 20, position: 'relative' },
+                    }}
+                    transition={{ duration: 0.15 }}
+                  >
+                    {title}
+                  </motion.span>
+                </motion.span>
               </Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </div>
