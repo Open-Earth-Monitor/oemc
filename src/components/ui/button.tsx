9f6674b8d3ba7e1e03828538bd63ee076374cce2
mluena <maluenarod@gmail.com>
2023-10-18 20:57:55 +0200

    geostories link animation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         default_active:
           'border-2 border-secondary-500 bg-secondary-500 text-brand-500 hover:bg-opacity-[0.1] hover:text-secondary-500 hover:border-secondary-500 hover:shadow-[-2px_2px_rgb(255, 255, 230)]',
         light:
-          'hover:shadow-[2px_2px_black] border-2 border-brand-500 bg-secondary-500 text-brand-500 hover:bg-opacity-[0.1] hover:text-secondary-500 hover:border-secondary-500 hover:shadow-[-2px_2px_rgb(255, 255, 230)]',
+          'border-2 border-brand-500 w-full font-bold bg-transparent text-sm font-inter  hover:shadow-[2px_2px_black]',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
         dark: 'w-full font-bold bg-brand-500 text-secondary-500 text-sm font-inter hover:bg-gray-900 hover:shadow-[2px_2px_black]',
