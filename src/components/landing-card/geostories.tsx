3c07fd9e1e0505fc6ce2be19adf87d02ac2d9a60
mluena <maluenarod@gmail.com>
2023-10-18 13:49:41 +0200

    card animation

--- src/components/landing-card/geostories.tsx
+++ src/components/landing-card/geostories.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import Link from 'next/link';
 
+import { AnimatePresence, motion } from 'framer-motion';
 import { HiOutlineArrowTopRightOnSquare } from 'react-icons/hi2';
 
 import cn from '@/lib/classnames';
@@ -15,42 +16,48 @@ const Card: FC<Partial<Geostory> & { color?: string; headColor?: string }> = ({
   color,
   headColor,
 }) => (
-  <div
-    className="min-h-[388px] w-[384px]"
-    style={{ backgroundColor: color }}
-    data-testid={`dataset-item-${id}`}
-  >
-    <div className="space-y-6 p-6 text-secondary-500" style={{ backgroundColor: headColor }}>
-      <div>
-        <span className="text-xs uppercase">geostories</span>
-        <h2 data-testid="dataset-title" className="font-satoshi text-2xl font-bold">
-          {title}
-        </h2>
+  <AnimatePresence>
+    <motion.div
+      className="min-h-[388px] w-[384px]"
+      style={{ backgroundColor: color }}
+      data-testid={`dataset-item-${id}`}
+      whileHover={{
+        translateY: '-10px',
+      }}
+      transition={{ duration: 0.1 }}
+    >
+      <div className="space-y-6 p-6 text-secondary-500" style={{ backgroundColor: headColor }}>
+        <div>
+          <span className="text-xs uppercase">geostories</span>
+          <h2 data-testid="dataset-title" className="font-satoshi text-2xl font-bold">
+            {title}
+          </h2>
+        </div>
+        <Link
+          href={`/map/geostories/${id}`}
+          data-testid="dataset-layer-toggle-button"
+          className={cn(
+            'flex items-center space-x-4 py-2 text-xs font-bold text-secondary-500 transition-colors hover:bg-secondary-500/20'
+          )}
+        >
+          <HiOutlineArrowTopRightOnSquare className="h-5 w-5" />
+          <span>Go to geostory</span>
+        </Link>
       </div>
-      <Link
-        href={`/map/geostories/${id}`}
-        data-testid="dataset-layer-toggle-button"
-        className={cn(
-          'flex items-center space-x-4 py-2 text-xs font-bold text-secondary-500 transition-colors hover:bg-secondary-500/20'
-        )}
-      >
-        <HiOutlineArrowTopRightOnSquare className="h-5 w-5" />
-        <span>Go to geostory</span>
-      </Link>
-    </div>
-    <div className="p-6">
-      <span className="text-xs font-medium uppercase">monitor</span>
-      <ul>
-        {layers.map(({ title }) => (
-          <li key={id}>
-            <Link href={`/map/${id}/datasets`} className="font-bold underline">
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
+      <div className="p-6">
+        <span className="text-xs font-medium uppercase">monitor</span>
+        <ul>
+          {layers.map(({ title }) => (
+            <li key={id}>
+              <Link href={`/map/${id}/datasets`} className="font-bold underline">
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </motion.div>
+  </AnimatePresence>
 );
 
 export default Card;
--- src/components/landing-card/monitors.tsx
+++ src/components/landing-card/monitors.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import Link from 'next/link';
 
+import { AnimatePresence, motion } from 'framer-motion';
+
 import cn from '@/lib/classnames';
 
 import { Monitor } from '@/types/monitors';
@@ -18,58 +20,64 @@ import {
 } from '@/components/ui/dialog';
 
 const Card: FC<Partial<Monitor> & { color?: string }> = ({ id, description, title, color }) => (
-  <div
-    className="h-[388px] w-[384px] space-y-6 p-6"
-    style={{ backgroundColor: color }}
-    data-testid={`dataset-item-${id}`}
-  >
-    <div>
-      <span className="text-xs uppercase">monitor</span>
-      <h2 data-testid="dataset-title" className="font-satoshi text-2xl font-bold">
-        {title}
-      </h2>
-    </div>
+  <AnimatePresence>
+    <motion.div
+      className="h-[388px] w-[384px] space-y-6 p-6"
+      style={{ backgroundColor: color }}
+      data-testid={`dataset-item-${id}`}
+      whileHover={{
+        translateY: '-10px',
+      }}
+      transition={{ duration: 0.1 }}
+    >
+      <div>
+        <span className="text-xs uppercase">monitor</span>
+        <h2 data-testid="dataset-title" className="font-satoshi text-2xl font-bold">
+          {title}
+        </h2>
+      </div>
 
-    <p data-testid="dataset-description">{description}</p>
+      <p data-testid="dataset-description">{description}</p>
 
-    <Dialog>
-      <DialogOverlay className="bg-brand-500 bg-opacity-50" />
-      <DialogTrigger className="w-full" asChild>
-        <button
-          type="button"
-          className={cn(
-            'flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
-          )}
-        >
-          Explore monitor
-        </button>
-      </DialogTrigger>
-      <DialogContent className="w-[665px] bg-white p-10 py-6 text-brand-500 ">
-        <DialogHeader className="space-y-4">
-          <DialogTitle asChild>
-            <header className="divide-x-secondary-500 divide-x">
-              <h2 className="inline-block pr-6 font-satoshi text-5xl font-bold">{title}</h2>
-              <div>{description}</div>
-            </header>
-          </DialogTitle>
-          <DialogDescription asChild>
-            <div className="">
-              <Link
-                href={`/map/${id}/datasets`}
-                data-testid="dataset-layer-toggle-button"
-                className={cn(
-                  'flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
-                )}
-              >
-                Launch monitor
-              </Link>
-              <DialogClose>close</DialogClose>
-            </div>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  </div>
+      <Dialog>
+        <DialogOverlay className="bg-brand-500 bg-opacity-50" />
+        <DialogTrigger className="w-full" asChild>
+          <button
+            type="button"
+            className={cn(
+              'flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
+            )}
+          >
+            Explore monitor
+          </button>
+        </DialogTrigger>
+        <DialogContent className="w-[665px] bg-white p-10 py-6 text-brand-500 ">
+          <DialogHeader className="space-y-4">
+            <DialogTitle asChild>
+              <header className="divide-x-secondary-500 divide-x">
+                <h2 className="inline-block pr-6 font-satoshi text-5xl font-bold">{title}</h2>
+                <div>{description}</div>
+              </header>
+            </DialogTitle>
+            <DialogDescription asChild>
+              <div className="">
+                <Link
+                  href={`/map/${id}/datasets`}
+                  data-testid="dataset-layer-toggle-button"
+                  className={cn(
+                    'flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
+                  )}
+                >
+                  Launch monitor
+                </Link>
+                <DialogClose>close</DialogClose>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </motion.div>
+  </AnimatePresence>
 );
 
 export default Card;
