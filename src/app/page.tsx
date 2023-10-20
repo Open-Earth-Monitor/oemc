acaad0d9d4011d4ccc7417365c3df56016c7e714
mluena <maluenarod@gmail.com>
2023-10-20 15:47:30 +0200

    search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 
 const Hub: NextPage = () => (
-  <div className=" text-secondary-500">
+  <div className="text-secondary-500">
     <Hero />
     <LandingDatasets />
     <Prefooter />
--- src/components/search/constants.ts
+++
@@ -1,4 +0,0 @@
-export const THEME = {
-  dark: 'text-white',
-  light: 'text-black',
-};
