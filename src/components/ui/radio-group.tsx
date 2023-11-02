5f75458a3073bcaaafa4d10b96f35e1543052000
mluena <maluenarod@gmail.com>
2023-11-02 12:35:11 +0100

    missing color in radio buttons

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -34,7 +34,7 @@ const RadioGroupItem = React.forwardRef<
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center rounded-full">
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center rounded-full text-secondary-500">
         <Circle className="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
