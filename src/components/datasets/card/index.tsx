9d50a878f2f5141a86cdab056f6af2f159d7a0af
mluena <maluenarod@gmail.com>
2023-11-08 10:08:07 +0100

    date back to url

--- src/components/datasets/card/index.tsx
+++ src/components/datasets/card/index.tsx
@@ -17,6 +17,7 @@ type DatasetCardProps = LayerParsed & {
   id: string;
   active?: boolean;
   autoPlay?: boolean;
+  defaultActive?: boolean;
 };
 
 const DatasetCard: FC<DatasetCardProps> = ({
@@ -27,61 +28,65 @@ const DatasetCard: FC<DatasetCardProps> = ({
   author,
   gs_style: legendStyles,
   range,
-  autoPlay = true,
-  active,
+  autoPlay = false,
+  defaultActive,
 }) => {
   const [layers, setLayers] = useSyncLayersSettings();
+  const [compareLayers, setCompareLayers] = useSyncCompareLayersSettings();
   const layerId = layers?.[0]?.id;
   const layerOpacity = layers?.[0]?.opacity;
   const layerDate = layers?.[0]?.date;
-  const [isActive, setIsActive] = useState<boolean>(active);
+  const [isActive, setIsActive] = useState<boolean>(defaultActive);
   const [isFirstRender, setIsFirstRender] = useState<boolean>(true);
+  const opacity = layerOpacity ?? 1;
+  const compareId = compareLayers?.[0]?.id;
 
   // activates first layer at first render
   useEffect(() => {
-    if (active && isFirstRender)
+    if (isActive && isFirstRender) {
       setLayers([
         {
-          opacity: layerOpacity ?? 1,
-          date: layerDate || range?.[0]?.value,
-          id: layerId || id,
+          opacity,
+          date: layerDate ?? range?.[0]?.value,
+          id: layerId ?? id,
         },
       ]);
+      if (!!compareId)
+        setCompareLayers([{ id: layerId, opacity, date: layerDate ?? range?.[0]?.value }]);
+    }
+    setIsFirstRender(false);
   }, []);
+
+  useEffect(() => {
+    if (isActive) {
+      setLayers([
+        {
+          opacity,
+          date: layerDate ?? range?.[0]?.value,
+          id: layerId ?? id,
+        },
+      ]);
+    }
+  }, [isActive]);
+
+  useEffect(() => {
+    if (!isActive && !isFirstRender) {
+      void setLayers(null);
+      void setCompareLayers(null);
+    }
+  }, [isActive, setLayers]);
 
   /**
    * At mount, if layerId is in the URL, update the URL with the layerId
    */
-  useEffect(() => {
-    if (active && isActive && layerId !== id) {
-      setLayers([
-        {
-          id,
-          opacity: !layerOpacity && layerOpacity !== 0 ? 1 : layerOpacity,
-          date: layerDate || range?.[0]?.value,
-        },
-      ]);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (isActive)
-      setLayers([
-        {
-          opacity: layerOpacity ?? 1,
-          date: layerDate || range?.[0]?.value,
-          id,
-        },
-      ]);
-  }, []);
 
   /**
    * Handle click on the toggle button
    */
+
   const handleClick = useCallback(() => {
-    setIsActive(isActive);
-  }, [id, setIsActive]);
+    setIsActive(!isActive);
+  }, [isActive]);
 
   return (
     <div className="space-y-6 bg-brand-300 p-6" data-testid={`dataset-item-${id}`}>
@@ -118,7 +123,7 @@ const DatasetCard: FC<DatasetCardProps> = ({
 
       <p data-testid="dataset-description">{description}</p>
 
-      {isActive && legendStyles && legendStyles.length > 8 && (
+      {layerId === id && legendStyles && legendStyles.length > 8 && (
         <div className="columns-2 gap-2 space-y-1">
           {legendStyles.map(({ color, label }) => (
             <div
@@ -138,7 +143,7 @@ const DatasetCard: FC<DatasetCardProps> = ({
         </div>
       )}
 
-      {isActive && legendStyles && legendStyles.length <= 8 && (
+      {layerId === id && legendStyles && legendStyles.length <= 8 && (
         <div className="flex">
           {legendStyles.map(({ color, label }) => (
             <div key={label} className="grow space-y-2" data-testid="dataset-legend-item">
@@ -153,8 +158,14 @@ const DatasetCard: FC<DatasetCardProps> = ({
           ))}
         </div>
       )}
-
-      {range && <TimeSeries range={range} layerId={id} autoPlay={autoPlay} isActive={isActive} />}
+      {range && (
+        <TimeSeries
+          range={range}
+          layerId={id}
+          autoPlay={defaultActive ?? autoPlay}
+          isActive={layerId === id}
+        />
+      )}
 
       <button
         data-testid="dataset-layer-toggle-button"
@@ -162,12 +173,12 @@ const DatasetCard: FC<DatasetCardProps> = ({
         className={cn(
           'flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-secondary-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20',
           {
-            'bg-secondary-500 text-brand-500 hover:text-secondary-500': isActive,
+            'bg-secondary-500 text-brand-500 hover:text-secondary-500': layerId === id,
           }
         )}
         onClick={() => handleClick()}
       >
-        <span>{isActive ? 'Hide' : 'Show'} layer on the map</span>
+        <span>{layerId === id ? 'Hide' : 'Show'} layer on the map</span>
         <LuLayers className="h-3 w-3 text-inherit" title="layer" />
       </button>
     </div>
