73b7ff0bdb3a5e72aa983cbafef61f815d943a3e
mluena <maluenarod@gmail.com>
2023-11-20 09:44:45 +0100

    hover btns

--- src/components/map/controls/constants.ts
+++ src/components/map/controls/constants.ts
@@ -1,9 +1,9 @@
 export const CONTROL_BUTTON_STYLES = {
   default:
-    'h-[34px] w-[34px] flex items-center justify-center rounded-sm bg-brand-400 text-secondary-500 active:bg-brand-200 active:text-secondary-500',
+    'h-[34px] w-[34px] flex items-center justify-center rounded-sm bg-brand-400 text-secondary-500 active:bg-brand-200 active:text-secondary-500 hover:bg-secondary-500 hover:text-brand-500',
   disabled: 'cursor-default opacity-50 pointer-events-none',
 };
 
 export const CONTROL_ICON_STYLES = {
-  default: 'h-4 w-4',
+  default: 'h-4 w-4 hover:bg-secondary-500 hover:text-brand-500',
 };
