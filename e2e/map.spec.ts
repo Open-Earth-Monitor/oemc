b9d482b8815b34ab890933e73c2eb34f40e535d3
mluena <maluenarod@gmail.com>
2023-11-20 08:48:34 +0100

    map info

--- e2e/map.spec.ts
+++ e2e/map.spec.ts
@@ -94,3 +94,85 @@ test('opacity 0.5 from url', async ({ page }) => {
   await page.getByTestId('map-legend-item').getByTestId('layer-opacity-button').click();
   await expect(page.getByTestId('slider-current-value')).toHaveText('50%');
 });
+
+test.describe('general information in map page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/map/m1/datasets', { waitUntil: 'load' });
+  });
+  test('attributtions', async ({ page }) => {
+    await page.goto('/map/m1/datasets', { waitUntil: 'load' });
+    const attributions = page.getByTestId('attributions');
+    await expect(attributions).toBeVisible();
+    await attributions.click();
+
+    const attributionsContent = page.getByTestId('attributions-content');
+    await expect(attributionsContent).toBeVisible();
+    await expect(attributionsContent).toHaveText(
+      "This project has received funding from the European Union's Horizon Europe research and innovation programme under grant agreement No. 101059548."
+    );
+  });
+
+  test('disclaimer', async ({ page }) => {
+    await page.goto('/map/m1/datasets', { waitUntil: 'load' });
+    const disclaimer = page.getByTestId('disclaimer');
+    await expect(disclaimer).toBeVisible();
+    await disclaimer.click();
+
+    const disclaimerContent = page.getByTestId('disclaimer-content');
+    await expect(disclaimerContent).toBeVisible();
+    await expect(page.getByTestId('disclaimer-content-1')).toHaveText(
+      'Funded by the European Union. Views and opinions expressed are however those of the author(s) only and do not necessarily reflect those of the European Union or European Commission.'
+    );
+    await expect(page.getByTestId('disclaimer-content-2')).toHaveText(
+      'Neither the European Union nor the granting authority can be held responsible for them. The data is provided “as is”. Open-Earth-Monitor Cyberinfrastructure (OEMC) project consortium and its suppliers and licensors hereby disclaim all warranties of any kind, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement.'
+    );
+    await expect(page.getByTestId('disclaimer-content-3')).toHaveText(
+      'Neither OEMC project Consortium nor its suppliers and licensors, makes any warranty that the Website will be error free or that access thereto will be continuous or uninterrupted. You understand that you download from, or otherwise obtain content or services through, the Website at your own discretion and risk.'
+    );
+  });
+
+  test('OEMC contact us', async ({ page }) => {
+    await page.goto('/map/m1/datasets', { waitUntil: 'load' });
+
+    const contactUs = page.getByTestId('contact-link');
+    await contactUs.click();
+    await expect(contactUs).toHaveAttribute('href', 'https://earthmonitor.org/contact-us/');
+  });
+
+  test('OEMC privacy policy', async ({ page }) => {
+    const privacyPolicy = page.getByTestId('privacy-policy-link');
+    await privacyPolicy.click();
+    await expect(privacyPolicy).toHaveAttribute('href', 'https://earthmonitor.org/privacy-policy/');
+  });
+});
+
+test.describe('social media', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/map/m1/datasets', { waitUntil: 'load' });
+    const socialMedia = page.getByTestId('social-media');
+    await socialMedia.click();
+  });
+  test('twitter', async ({ page }) => {
+    const twitterLink = page.getByTestId('twitter-link');
+    await expect(twitterLink).toHaveAttribute('href', `https://twitter.com/EarthMonitorOrg`);
+  });
+  test('linkedin', async ({ page }) => {
+    const linkedinLink = page.getByTestId('linkedin-link');
+    await expect(linkedinLink).toHaveAttribute('href', `https://www.linkedin.com/in/opengeohub`);
+  });
+  // test('mastodon', async ({ page }) => {
+  //   const mastodonLink = page.getByTestId('mastodon-link');
+  //   await expect(mastodonLink).toHaveAttribute('href', ``);
+  // });
+  test('github', async ({ page }) => {
+    const githubLink = page.getByTestId('github-link');
+    await expect(githubLink).toHaveAttribute('href', `https://github.com/Open-Earth-Monitor`);
+  });
+  test('tib', async ({ page }) => {
+    const tibLink = page.getByTestId('tib-link');
+    await expect(tibLink).toHaveAttribute(
+      'href',
+      `https://av.tib.eu/publisher/OpenGeoHub_Foundation`
+    );
+  });
+});
--- src/components/monitors/dialog/index.tsx
+++ src/components/monitors/dialog/index.tsx
@@ -33,7 +33,7 @@ const MonitorsDirectoryDialog: FC = () => {
           <DialogDescription asChild>
             <div>
               <MonitorsTable />
-              <DialogClose className="absolute right-10 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+              <DialogClose className="absolute right-10 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
             </div>
           </DialogDescription>
         </DialogHeader>
