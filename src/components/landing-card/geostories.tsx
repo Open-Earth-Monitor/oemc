fad6275e8b926b1217f87a035e1b801677ff5ad6
mluena <maluenarod@gmail.com>
2023-11-21 11:57:57 +0100

    prevent app from crashing when there is no layer

--- src/components/landing-card/geostories.tsx
+++ src/components/landing-card/geostories.tsx
@@ -43,44 +43,50 @@ const Card: FC<Partial<Geostory> & { color?: string; headColor?: string }> = ({
         <span className="hover:underline ">Go to geostory</span>
       </Link>
     </div>
-    <div className="px-10 py-4 text-brand-500">
-      <span className={TAG_STYLE}>monitor</span>
-      <motion.div
-        className="opacity-1 relative flex items-center"
-        initial="initial"
-        whileHover="hover"
-        transition={{ duration: 0.3 }}
-      >
-        <Link href={`/map/${layers[0].layer_id}/datasets`} className="w-full font-bold underline">
-          <motion.span
-            variants={{
-              initial: {
-                opacity: 0,
-              },
-              hover: {
-                opacity: 1,
-              },
-            }}
+    {/* TO - DO - This should be a list of monitors, not layers. Implemented just for visualization purposes. Remove when API returns the monitor the geo story belongs to in the endpoint */}
+    {!!layers.length && (
+      <div className="px-10 py-4 text-brand-500">
+        <span className={TAG_STYLE}>monitor</span>
+        <motion.div
+          className="opacity-1 relative flex items-center"
+          initial="initial"
+          whileHover="hover"
+          transition={{ duration: 0.3 }}
+        >
+          <Link
+            href={`/map/${layers[0]?.layer_id}/datasets`}
+            className="w-full font-bold underline"
           >
-            <HiArrowRight className="absolute left-0 top-2 inline-block h-5 w-5 fill-current" />
-          </motion.span>
-          <motion.span
-            className="absolute left-0 top-2 inline-flex whitespace-normal pb-11 underline"
-            variants={{
-              initial: {
-                x: 0,
-              },
-              hover: {
-                x: 30,
-              },
-            }}
-            transition={{ duration: 0.25 }}
-          >
-            {layers[0].title}
-          </motion.span>
-        </Link>
-      </motion.div>
-    </div>
+            <motion.span
+              variants={{
+                initial: {
+                  opacity: 0,
+                },
+                hover: {
+                  opacity: 1,
+                },
+              }}
+            >
+              <HiArrowRight className="absolute left-0 top-2 inline-block h-5 w-5 fill-current" />
+            </motion.span>
+            <motion.span
+              className="absolute left-0 top-2 inline-flex whitespace-normal pb-11 underline"
+              variants={{
+                initial: {
+                  x: 0,
+                },
+                hover: {
+                  x: 30,
+                },
+              }}
+              transition={{ duration: 0.25 }}
+            >
+              {layers[0]?.title}
+            </motion.span>
+          </Link>
+        </motion.div>
+      </div>
+    )}
   </div>
 );
 
