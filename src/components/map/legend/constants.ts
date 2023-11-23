d6f3dc6a16bb3ee6e16bff9c40f4e0c80316ea4b
mluena <maluenarod@gmail.com>
2023-11-23 11:42:46 +0100

    scroll to dropdowns

--- src/components/map/legend/constants.ts
+++ src/components/map/legend/constants.ts
@@ -1,4 +1,7 @@
 export const DROPDOWN_TRIGGER_STYLES = 'w-full border-none px-3.5 py-2.5';
-export const DROPDOWN_CONTENT_STYLES = 'z-[60] max-h-56 w-full overflow-y-auto bg-brand-500';
+export const DROPDOWN_CONTENT_STYLES = 'dropdown-menu-content z-[60] w-full bg-brand-500 px-0';
+export const DROPDOWN_ITEM_STYLES =
+  'm-auto flex w-full flex-1 justify-center whitespace-nowrap py-0 px-2 text-center text-secondary-500 ';
+
 export const LEGEND_BUTTON_STYLES =
   'bg-brand-500 flex-1 text-center text-xs uppercase font-medium grow px-2 h-[34px] py-1 text-white hover:bg-secondary-500 hover:text-brand-500 disabled:opacity-50 disabled:cursor-not-allowed';
