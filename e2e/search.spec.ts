2188910fbed789d0bc1ba91ac91ac209cb88aefa
David Inga <david.inga@vizzuality.com>
2023-11-29 10:26:05 +0100

    fixed search

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 import type { Geostory } from '@/types/geostories';
 import type { Monitor } from '@/types/monitors';
 
+import type { PaginatedResponse } from '@/hooks/datasets';
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
@@ -10,36 +12,38 @@ test.beforeEach(async ({ page }) => {
 test.describe('search of monitors and geostories', () => {
   test('search by title', async ({ page }) => {
     const response = await page.waitForResponse(
-      'https://api.earthmonitor.org/monitors-and-geostories?sort_by=title'
+      'https://api.earthmonitor.org/monitors-and-geostories*'
     );
-    const datasetsData = (await response.json()) as (Monitor | Geostory)[];
+    const datasetsData = (await response.json()) as PaginatedResponse;
     const searchInput = page.getByTestId('search-input');
 
     const searchPromise = page.waitForResponse(
-      'https://api.earthmonitor.org/monitors-and-geostories?title=*&sort_by=title'
+      'https://api.earthmonitor.org/monitors-and-geostories?*title=*'
     );
-    await searchInput.fill(datasetsData[0].title);
+    await searchInput.fill(datasetsData['monitors and geostories'][0].title);
     const filteredResponse = await searchPromise;
     const inputValue = await searchInput.inputValue();
-    const filteredJson = (await filteredResponse.json()) as (Monitor | Geostory)[];
+    const filteredJson = (await filteredResponse.json()) as PaginatedResponse;
 
     // compare filtered response with manually filtered response
-    const manuallyFilteredResponse = datasetsData.filter((dataset) => dataset.title === inputValue);
-    expect(filteredJson).toEqual(manuallyFilteredResponse);
+    const manuallyFilteredResponse = datasetsData['monitors and geostories'].filter(
+      (dataset) => dataset.title === inputValue
+    );
+    expect(filteredJson['monitors and geostories']).toEqual(manuallyFilteredResponse);
 
     // check that the number of results is displayed accurately
     await expect(page.getByTestId('datasets-result')).toBeVisible();
-    if (filteredJson.length === 1) {
+    if (filteredJson['monitors and geostories'].length === 1) {
       await expect(page.getByTestId('datasets-result')).toHaveText('1 result');
-    } else if (filteredJson.length > 1) {
+    } else if (filteredJson['monitors and geostories'].length > 1) {
       await expect(page.getByTestId('datasets-result')).toHaveText(
-        `${filteredJson.length} results`
+        `${filteredJson['monitors and geostories'].length} results`
       );
     }
 
     // check that the no results disclaimer is displayed
     const noResultsDisclaimer = page.getByTestId('no-results-found');
-    if (!filteredJson.length) {
+    if (!filteredJson['monitors and geostories'].length) {
       await expect(noResultsDisclaimer).toBeVisible();
     }
   });
