d4b2de1edb31afc5a1352e770e00532926214ad9
David Inga <david.inga@vizzuality.com>
2023-11-29 10:47:44 +0100

    fixed test in map

--- src/components/map/legend/opacity/index.tsx
+++ src/components/map/legend/opacity/index.tsx
@@ -11,7 +11,7 @@ import { Popover, PopoverArrow, PopoverContent, PopoverTrigger } from '@/compone
 
 export const OpacitySetting: FC = () => {
   const [layers, setLayers] = useSyncLayersSettings();
-  const [opacityContent, setOpacityContent] = useState<'info' | 'slider'>('info');
+  // const [opacityContent, setOpacityContent] = useState<'info' | 'slider'>('info');
   const [isOpacityPopoverOpen, setOpacityPopoverOpen] = useState<boolean>(false);
 
   const layerOpacity = layers?.[0]?.opacity;
@@ -26,17 +26,17 @@ export const OpacitySetting: FC = () => {
     [setLayers]
   );
 
-  const handleMouseLeave = useCallback(() => {
-    setOpacityContent('slider');
-  }, []);
+  // const handleMouseLeave = useCallback(() => {
+  //   setOpacityContent('slider');
+  // }, []);
 
-  const handleMouseEnter = useCallback(() => {
-    setOpacityContent('info');
-  }, []);
+  // const handleMouseEnter = useCallback(() => {
+  //   setOpacityContent('info');
+  // }, []);
 
   return (
     <Popover onOpenChange={handleOpacityVisibility}>
-      <PopoverTrigger data-testid="layer-opacity-button" onMouseEnter={handleMouseEnter}>
+      <PopoverTrigger data-testid="layer-opacity-button">
         <MdOutlineOpacity
           className={cn({
             'h-4 w-4 text-gray-600 hover:text-secondary-500': true,
@@ -45,36 +45,34 @@ export const OpacitySetting: FC = () => {
         />
       </PopoverTrigger>
       <PopoverContent
-        onClick={handleMouseLeave}
+        // onClick={handleMouseLeave}
         sideOffset={0}
         alignOffset={0}
         align="center"
         className={cn({
           'z-[60] w-[189px] rounded-3xl border border-secondary-900 px-3 py-3.5': true,
-          'p2 w-fit py-1': opacityContent === 'info',
+          // 'p2 w-fit py-1': opacityContent === 'info',
         })}
       >
-        {opacityContent === 'slider' && (
-          <div className="flex w-full flex-col space-y-2">
-            <div
-              data-testid="slider-current-value"
-              className="m-auto w-[55px] rounded-xl border border-secondary-900 px-3 py-2 font-inter text-xs font-medium text-white"
-            >
-              {Math.round(opacity * 100)}%
-            </div>
-            <div className="relative py-1.5">
-              <Slider
-                onValueChange={handleChange}
-                value={[opacity]}
-                min={0}
-                max={1}
-                step={0.01}
-                data-testid="layer-opacity-slider"
-              />
-            </div>
+        <div className="flex w-full flex-col space-y-2">
+          <div
+            data-testid="slider-current-value"
+            className="m-auto w-[55px] rounded-xl border border-secondary-900 px-3 py-2 font-inter text-xs font-medium text-white"
+          >
+            {Math.round(opacity * 100)}%
           </div>
-        )}
-        {opacityContent === 'info' && <div className="font-medium text-secondary-500">Opacity</div>}
+          <div className="relative py-1.5">
+            <Slider
+              onValueChange={handleChange}
+              value={[opacity]}
+              min={0}
+              max={1}
+              step={0.01}
+              data-testid="layer-opacity-slider"
+            />
+          </div>
+        </div>
+        {/* {opacityContent === 'info' && <div className="font-medium text-secondary-500">Opacity</div>} */}
         <PopoverArrow className="text-brand-50" />
       </PopoverContent>
     </Popover>
