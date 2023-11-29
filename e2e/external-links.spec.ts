7b4636eec56bcb55279c76849dc3dc8a6ba29dd2
David Inga <david.inga@vizzuality.com>
2023-11-29 09:59:35 +0100

    fixed external link tests

--- e2e/external-links.spec.ts
+++ e2e/external-links.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/', { waitUntil: 'load' });
@@ -7,38 +7,38 @@ test.beforeEach(async ({ page }) => {
 const socialLinks = [
   {
     test: 'twitter-link',
-    href: 'https://twitter.com/i/flow/login?redirect_after_login=%2FEarthMonitorOrg',
+    href: 'https://twitter.com/EarthMonitorOrg',
   },
-  // mastodon: '',
   { test: 'github-link', href: 'https://github.com/Open-Earth-Monitor' },
-  // { test: 'tib-link', href: 'https://av.tib.eu/publisher/OpenGeoHub_Foundation' },
-  { test: 'linkedin-link', href: 'https://www.linkedin.com/in/opengeohub]' },
+  { test: 'tib-link', href: 'https://av.tib.eu/publisher/OpenGeoHub_Foundation' },
+  { test: 'linkedin-link', href: 'https://www.linkedin.com/in/opengeohub' },
 ];
 
 test.describe('external links', () => {
   test('links to social media', async ({ page }) => {
     for (const link of socialLinks) {
       const Link = page.getByTestId(link['test']);
-      await Link.click();
-      await page.waitForURL(link.href);
+      const href = await Link.getAttribute('href');
+      expect(href).toBe(link['href']);
     }
   });
 
-  test('OEMC factsheet', async ({ page }) => {
-    const OEMCFactsheet = page.getByTestId('OEMC-factsheet-link');
-    await OEMCFactsheet.click();
-    await page.waitForURL('https://cordis.europa.eu/project/id/101059548');
+  test('OEMC disclaimer', async ({ page }) => {
+    const disclaimerButton = page.getByTestId('disclaimer');
+    await disclaimerButton.click();
+    // ensure a dialog is opened
+    await page.waitForSelector('div[role="dialog"]');
   });
 
-  test('OEMC conact us', async ({ page }) => {
-    const OEMCFactsheet = page.getByTestId('contact-link');
-    await OEMCFactsheet.click();
-    await page.waitForURL('https://earthmonitor.org/contact-us/');
+  test('OEMC contact us', async ({ page }) => {
+    const contactLink = page.getByTestId('contact-link');
+    const href = await contactLink.getAttribute('href');
+    expect(href).toBe('https://earthmonitor.org/contact-us/');
   });
 
   test('OEMC privacy policy', async ({ page }) => {
-    const OEMCFactsheet = page.getByTestId('privacy-policy-link');
-    await OEMCFactsheet.click();
-    await page.waitForURL('https://earthmonitor.org/privacy-policy/');
+    const privacyPolicyLink = page.getByTestId('privacy-policy-link');
+    const href = await privacyPolicyLink.getAttribute('href');
+    expect(href).toBe('https://earthmonitor.org/privacy-policy/');
   });
 });
