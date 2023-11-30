3b353c0eafa2cf996a6e7ea42ac0bd86ff213014
David Inga <david.inga@vizzuality.com>
2023-11-30 12:05:09 +0100

    added under-development tag

--- src/components/geostories/card/index.tsx
+++ src/components/geostories/card/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { HiOutlineNewspaper, HiOutlineGlobeAlt } from 'react-icons/hi';
 import { HiOutlineArrowTopRightOnSquare, HiArrowRight } from 'react-icons/hi2';
+import { LuRefreshCcw } from 'react-icons/lu';
 
 import cn from '@/lib/classnames';
 
@@ -31,6 +32,7 @@ const GeostoryCard: FC<Partial<Geostory> & { color?: string; headColor?: string 
   layers,
   color,
   headColor,
+  ready,
 }) => (
   <div
     className="min-h-[388px] w-[384px]"
@@ -46,96 +48,108 @@ const GeostoryCard: FC<Partial<Geostory> & { color?: string; headColor?: string 
           {title}
         </h2>
       </div>
-      <div className="flex items-center space-x-2">
-        <Dialog>
-          <DialogOverlay className="bg-brand-500 bg-opacity-50" />
-          <DialogTrigger asChild>
-            <Button data-testid={`card-button-${id}`} className="max-w-fit p-4">
-              Known more
-            </Button>
-          </DialogTrigger>
-          <DialogContent
-            data-testid={`monitor-card-${id}`}
-            className="w-[665px] bg-secondary-500 text-brand-500"
-          >
-            <DialogHeader className="space-y-6">
-              <DialogTitle asChild>
-                <header className="space-y-4">
-                  <h2 data-testid="monitor-title" className="inline-block pr-6 text-6xl font-bold">
-                    {title}
-                  </h2>
-                  <div data-testid="monitor-description" className="font-inter leading-[25px]">
-                    {description ||
-                      'Long description lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'}
-                  </div>
-                </header>
-              </DialogTitle>
-              <DialogDescription asChild>
-                <div className="font-inter">
-                  <div className="space-y-5 border-y border-brand-500 py-3">
-                    <p>
-                      <span className="pr-2.5 font-bold">Developed by:</span>
-                      {author || 'Lorem Ipsum'}
-                    </p>
-                    <p>
-                      <span className="pr-2.5 font-bold">Monitor tool website:</span>
-                      www.toolname.com
-                    </p>
-                    <p>
-                      <span className="pr-2.5 font-bold">Output format:</span>GeoTiff, gpkg
-                    </p>
-                    <p>
-                      <span className="pr-2.5 font-bold">Spatial resolution:</span>Lorem Ipsum
-                    </p>
-                    <p>
-                      <span className="pr-2.5 font-bold">Temporal resolution:</span>Lorem Ipsum
-                    </p>
-                  </div>
-                  <div>
-                    <Link
-                      href=""
-                      className="flex items-center space-x-2 border-b border-brand-500 py-5 text-2xl font-bold"
+      {ready ? (
+        <div className="flex items-center space-x-2">
+          <Dialog>
+            <DialogOverlay className="bg-brand-500 bg-opacity-50" />
+            <DialogTrigger asChild>
+              <Button data-testid={`card-button-${id}`} className="max-w-fit p-4">
+                Known more
+              </Button>
+            </DialogTrigger>
+            <DialogContent
+              data-testid={`monitor-card-${id}`}
+              className="w-[665px] bg-secondary-500 text-brand-500"
+            >
+              <DialogHeader className="space-y-6">
+                <DialogTitle asChild>
+                  <header className="space-y-4">
+                    <h2
+                      data-testid="monitor-title"
+                      className="inline-block pr-6 text-6xl font-bold"
                     >
-                      <HiOutlineNewspaper />
-                      <span>Publications</span>
-                    </Link>
-                    <Link
-                      href=""
-                      className="flex items-center space-x-2 border-b border-brand-500 py-5 text-2xl font-bold"
-                    >
-                      <HiOutlineGlobeAlt />
-                      <span>Use cases</span>
-                    </Link>
-                  </div>
+                      {title}
+                    </h2>
+                    <div data-testid="monitor-description" className="font-inter leading-[25px]">
+                      {description ||
+                        'Long description lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'}
+                    </div>
+                  </header>
+                </DialogTitle>
+                <DialogDescription asChild>
+                  <div className="font-inter">
+                    <div className="space-y-5 border-y border-brand-500 py-3">
+                      <p>
+                        <span className="pr-2.5 font-bold">Developed by:</span>
+                        {author || 'Lorem Ipsum'}
+                      </p>
+                      <p>
+                        <span className="pr-2.5 font-bold">Monitor tool website:</span>
+                        www.toolname.com
+                      </p>
+                      <p>
+                        <span className="pr-2.5 font-bold">Output format:</span>GeoTiff, gpkg
+                      </p>
+                      <p>
+                        <span className="pr-2.5 font-bold">Spatial resolution:</span>Lorem Ipsum
+                      </p>
+                      <p>
+                        <span className="pr-2.5 font-bold">Temporal resolution:</span>Lorem Ipsum
+                      </p>
+                    </div>
+                    <div>
+                      <Link
+                        href=""
+                        className="flex items-center space-x-2 border-b border-brand-500 py-5 text-2xl font-bold"
+                      >
+                        <HiOutlineNewspaper />
+                        <span>Publications</span>
+                      </Link>
+                      <Link
+                        href=""
+                        className="flex items-center space-x-2 border-b border-brand-500 py-5 text-2xl font-bold"
+                      >
+                        <HiOutlineGlobeAlt />
+                        <span>Use cases</span>
+                      </Link>
+                    </div>
 
-                  <Link
-                    href={`/map/${id}/datasets`}
-                    data-testid="monitor-button"
-                    className={cn(
-                      'mt-3 flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
-                    )}
-                  >
-                    Launch monitor
-                  </Link>
-                  <DialogClose className="right-10 top-10 flex h-4 items-center space-x-2 text-xs font-medium uppercase tracking-[0.96px] text-brand-500">
-                    Close
-                  </DialogClose>
-                </div>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-        <Link
-          href={`/map/geostories/${id}`}
-          data-testid={`card-link-${id}`}
-          className={cn(
-            'flex items-center space-x-2.5 py-2 text-xs font-bold transition-colors hover:underline'
-          )}
-        >
-          <HiOutlineArrowTopRightOnSquare className="h-5 w-5" />
-          <span className="hover:underline ">Go to geostory</span>
-        </Link>
-      </div>
+                    <Link
+                      href={`/map/${id}/datasets`}
+                      data-testid="monitor-button"
+                      className={cn(
+                        'mt-3 flex min-h-[38px] w-full items-center justify-center space-x-2 border-2 border-brand-500 px-6 py-2 text-xs font-bold transition-colors hover:bg-secondary-500/20'
+                      )}
+                    >
+                      Launch monitor
+                    </Link>
+                    <DialogClose className="right-10 top-10 flex h-4 items-center space-x-2 text-xs font-medium uppercase tracking-[0.96px] text-brand-500">
+                      Close
+                    </DialogClose>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <Link
+            href={`/map/geostories/${id}`}
+            data-testid={`card-link-${id}`}
+            className={cn(
+              'flex items-center space-x-2.5 py-2 text-xs font-bold transition-colors hover:underline'
+            )}
+          >
+            <HiOutlineArrowTopRightOnSquare className="h-5 w-5" />
+            <span className="hover:underline ">Go to geostory</span>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <div className="float-left flex items-center space-x-2 rounded-md bg-black/10 px-3 py-2 leading-none">
+            <LuRefreshCcw className="h-5 w-5" />
+            <span className="text-xs">Geostory under-development</span>
+          </div>
+        </div>
+      )}
     </div>
     {/* TO - DO - This should be a list of monitors, not layers. Implemented just for visualization purposes. Remove when API returns the monitor the geo story belongs to in the endpoint */}
     {!!layers.length && (
