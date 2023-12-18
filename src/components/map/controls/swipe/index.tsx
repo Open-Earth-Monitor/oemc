1d9972e9e958234f062be7db3c021edb0ca85dfa
David Inga <david.inga@vizzuality.com>
2023-12-18 23:12:40 +0100

    fixed control stuck for comparison in geostories

--- src/components/map/controls/swipe/index.tsx
+++ src/components/map/controls/swipe/index.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-import type { Control } from 'ol/control';
 import type BaseEvent from 'ol/events/Event';
 import Swipe from 'ol-ext/control/Swipe';
 import { useOL } from 'rlayers';
@@ -8,7 +7,7 @@ import { useOL } from 'rlayers';
 import 'ol-ext/dist/ol-ext.css';
 import { useSyncLayersSettings, useSyncCompareLayersSettings } from '@/hooks/sync-query';
 
-let swipeControl: Control | null = null; // control instance
+const swipeControl = new Swipe();
 
 const SwipeControl = () => {
   const [position, setPosition] = useState<number>(0.5);
@@ -16,30 +15,30 @@ const SwipeControl = () => {
 
   const [layersUrl] = useSyncLayersSettings();
   const [layersUrlCompare] = useSyncCompareLayersSettings();
+  const handleMoving = useCallback((e: BaseEvent & { position: number[] }) => {
+    setPosition(e.position[0]);
+  }, []);
 
   useEffect(() => {
     const layers = map.getAllLayers();
 
-    if (layers.length && layers[1] && layers[2] && !swipeControl) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      swipeControl = new Swipe({
-        layers: [layers[1]],
-        rightLayers: [layers[2]],
-        position,
-      }) as Control;
-      map.addControl(swipeControl);
-      swipeControl.addEventListener('moving', (e: BaseEvent & { position: number[] }) => {
-        setPosition(e.position[0]);
-      });
+    if (layers.length && layers[1] && layers[2]) {
+      swipeControl.setProperties({ position });
+      swipeControl.addLayer([layers[2]], true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [layersUrl, layersUrlCompare]);
+
+  useEffect(() => {
+    map.addControl(swipeControl);
+    swipeControl.addEventListener('moving', handleMoving);
+
     return () => {
-      if (swipeControl) {
-        map.removeControl(swipeControl);
-        swipeControl = null;
-      }
+      map.removeControl(swipeControl);
+      swipeControl.removeEventListener('moving', handleMoving);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [map, layersUrl, layersUrlCompare]);
+  }, []);
 
   return null;
 };
