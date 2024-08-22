c9a81552cc4c2d3b5fbef7a78ab594b4e363280b
María <maluenarod@gmail.com>
2024-08-22 12:31:05 +0200

    gesotories mobile

--- src/components/geostories/content/index.tsx
+++ src/components/geostories/content/index.tsx
@@ -51,6 +51,12 @@ const GeostoryContent = ({ children }: PropsWithChildren) => {
     void setOpen((prev) => !prev);
   };
 
+  const onOpenChangeMobile = () => {
+    if (open) {
+      void setOpen(false);
+    }
+  };
+
   const [layers, setLayers] = useSyncLayersSettings();
   const [compareLayers, setCompareLayers] = useSyncCompareLayersSettings();
 
@@ -88,25 +94,88 @@ const GeostoryContent = ({ children }: PropsWithChildren) => {
   }, [geostoryLayers, comparisonLayer]);
 
   return (
-    <div className="relative">
-      <section className="absolute bottom-0 left-0 z-[55] w-full border-t border-secondary-900 bg-brand-500 p-1 sm:bottom-auto sm:left-4 sm:top-[82px] sm:w-fit sm:border-0 sm:p-0">
-        {/* Desktop */}
-        <Sheet onOpenChange={onOpenChange} open={defaultOpen && open}>
-          <SheetTrigger
-            className={cn({
-              'hidden h-[60px] w-12 border-none bg-brand-500': true,
-              'sm:block': !open,
-              hidden: open,
-            })}
-          >
-            <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
-          </SheetTrigger>
-          <SheetContent
-            side="left"
-            className="bottom-3 left-4 top-auto hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)]"
-          >
-            <div className="bottom-3 left-4 hidden h-[calc(100vh-24px-70px)] w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 lg:block">
-              <ScrollArea className="h-full w-[526px] p-7.5" type="auto">
+    <>
+      <div className="relative">
+        <section className="absolute bottom-0 left-0 z-[55] w-full border-t border-secondary-900 bg-brand-500 p-1 sm:bottom-auto sm:left-4 sm:top-[82px] sm:w-fit sm:border-0 sm:p-0">
+          {/* Desktop */}
+          <Sheet onOpenChange={onOpenChange} open={defaultOpen && open}>
+            <SheetTrigger
+              className={cn({
+                'hidden h-[60px] w-12 border-none bg-brand-500': true,
+                'sm:block': !open,
+                hidden: open,
+              })}
+            >
+              <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="bottom-3 left-4 top-auto hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)]"
+            >
+              <div className="bottom-3 left-4 hidden h-[calc(100vh-24px-70px)] w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 lg:block">
+                <ScrollArea className="h-full w-[526px] p-7.5" type="auto">
+                  <div className="space-y-6">
+                    <div className="divide-y divide-secondary-900">
+                      {geostoryData?.monitors?.[0].id && (
+                        <Link
+                          href={`/map/${geostoryData.monitors[0].id}/geostories`}
+                          className="sticky top-0 z-10 block space-x-3 bg-brand-500 pb-8 font-bold"
+                          data-testid="back-to-monitor"
+                          style={{ color: geostoryData.color }}
+                        >
+                          <HiArrowLeft className="inline-block h-6 w-6" />
+                          <span data-testid="monitor-title-back-btn">
+                            Back to {geostoryData.monitors[0].title}.
+                          </span>
+                        </Link>
+                      )}
+                      {isGeostoryLoading && <Loading />}
+                      {geostoryData && !isGeostoryLoading && (
+                        <GeostoryHeader {...geostoryData} color={geostoryData.color} />
+                      )}
+                    </div>
+                    <div>
+                      {isLayersLoading && <Loading />}-
+                      {!!layersData?.length && !isLayersLoading && (
+                        <ul className="space-y-6" data-testid="datasets-list">
+                          {geostoryLayers.map((dataset) => (
+                            <li key={dataset.layer_id}>
+                              <DatasetCard
+                                {...dataset}
+                                type="geostory"
+                                id={dataset.layer_id}
+                                isGeostory
+                              />
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                  {children}
+                </ScrollArea>
+              </div>
+              <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
+                <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
+              </SheetClose>
+            </SheetContent>
+          </Sheet>
+
+          {/* Tablet */}
+          <Sheet onOpenChange={onOpenChange} open={isTablet && defaultOpen && open}>
+            <SheetTrigger
+              className={cn({
+                'hidden h-[60px] w-12 border-none bg-brand-500': true,
+                hidden: open,
+              })}
+            >
+              <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="bottom-3 left-4 top-auto z-[60] hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)] lg:hidden"
+            >
+              <ScrollArea className="h-full w-[370px] p-7.5" type="auto">
                 <div className="space-y-6">
                   <div className="divide-y divide-secondary-900">
                     {geostoryData?.monitors?.[0].id && (
@@ -147,133 +216,75 @@ const GeostoryContent = ({ children }: PropsWithChildren) => {
                 </div>
                 {children}
               </ScrollArea>
-            </div>
-            <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
-              <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
-            </SheetClose>
-          </SheetContent>
-        </Sheet>
-
-        {/* Tablet */}
-        <Sheet onOpenChange={onOpenChange} open={isTablet && defaultOpen && open}>
-          <SheetTrigger
-            className={cn({
-              'hidden h-[60px] w-12 border-none bg-brand-500': true,
-              hidden: open,
-            })}
-          >
-            <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
-          </SheetTrigger>
-          <SheetContent
-            side="left"
-            className="bottom-3 left-4 top-auto z-[60] hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)] lg:hidden"
-          >
-            <ScrollArea className="h-full w-[370px] p-7.5" type="auto">
-              <div className="space-y-6">
-                <div className="divide-y divide-secondary-900">
-                  {geostoryData?.monitors?.[0].id && (
-                    <Link
-                      href={`/map/${geostoryData.monitors[0].id}/geostories`}
-                      className="sticky top-0 z-10 block space-x-3 bg-brand-500 pb-8 font-bold"
-                      data-testid="back-to-monitor"
-                      style={{ color: geostoryData.color }}
-                    >
-                      <HiArrowLeft className="inline-block h-6 w-6" />
-                      <span data-testid="monitor-title-back-btn">
-                        Back to {geostoryData.monitors[0].title}.
-                      </span>
-                    </Link>
-                  )}
-                  {isGeostoryLoading && <Loading />}
-                  {geostoryData && !isGeostoryLoading && (
-                    <GeostoryHeader {...geostoryData} color={geostoryData.color} />
-                  )}
-                </div>
-                <div>
-                  {isLayersLoading && <Loading />}-
-                  {!!layersData?.length && !isLayersLoading && (
-                    <ul className="space-y-6" data-testid="datasets-list">
-                      {geostoryLayers.map((dataset) => (
-                        <li key={dataset.layer_id}>
-                          <DatasetCard
-                            {...dataset}
-                            type="geostory"
-                            id={dataset.layer_id}
-                            isGeostory
-                          />
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              </div>
-              {children}
-            </ScrollArea>
-            <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
-              <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
-            </SheetClose>
-          </SheetContent>
-        </Sheet>
-
-        <Popover onOpenChange={onOpenChange} open={isMobile && defaultOpen && open}>
-          <PopoverTrigger className="z-[60] h-12 w-1/2 bg-secondary-500 font-inter text-sm font-medium uppercase text-brand-500 hover:bg-secondary-900 hover:text-secondary-500 data-[state=open]:bg-secondary-900 data-[state=open]:text-secondary-500 sm:hidden">
-            Geostory
-          </PopoverTrigger>
-          <PopoverContent
-            sideOffset={0}
-            side="top"
-            className="w-screen rounded-none border-none px-0 py-0"
-          >
-            <PopoverClose className="absolute left-0 top-0 block h-12 w-[60px] -translate-y-full border-none bg-brand-500 focus:text-secondary-500">
-              <ChevronDown className="mx-auto h-6 w-6 text-secondary-500" />
-            </PopoverClose>
-            <ScrollArea className="h-full max-h-[60vh] w-full" type="auto">
-              <div className="w-full space-y-1 sm:space-y-6">
-                <div className="space-y-6">
-                  <div className="divide-y divide-secondary-900">
-                    {geostoryData?.monitors?.[0].id && (
-                      <Link
-                        href={`/map/${geostoryData.monitors[0].id}/geostories`}
-                        className="sticky top-0 z-10 block space-x-3 bg-brand-500 pb-8 font-bold"
-                        data-testid="back-to-monitor"
-                        style={{ color: geostoryData.color }}
-                      >
-                        <HiArrowLeft className="inline-block h-6 w-6" />
-                        <span data-testid="monitor-title-back-btn">
-                          Back to {geostoryData.monitors[0].title}.
-                        </span>
-                      </Link>
-                    )}
-                    {isGeostoryLoading && <Loading />}
-                    {geostoryData && !isGeostoryLoading && (
-                      <GeostoryHeader {...geostoryData} color={geostoryData.color} />
-                    )}
-                  </div>
-                  <div>
-                    {isLayersLoading && <Loading />}-
-                    {!!layersData?.length && !isLayersLoading && (
-                      <ul className="space-y-6" data-testid="datasets-list">
-                        {geostoryLayers.map((dataset) => (
-                          <li key={dataset.layer_id}>
-                            <DatasetCard
-                              {...dataset}
-                              type="geostory"
-                              id={dataset.layer_id}
-                              isGeostory
-                            />
-                          </li>
-                        ))}
-                      </ul>
-                    )}
+              <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
+                <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
+              </SheetClose>
+            </SheetContent>
+          </Sheet>
+        </section>
+      </div>
+      {isMobile && (
+        <div className="absolute bottom-0 left-0 right-0 z-[500] h-[58px] bg-brand-500 px-1 py-2">
+          <Popover onOpenChange={onOpenChangeMobile} open={isMobile && defaultOpen && open}>
+            <PopoverTrigger className="absolute bottom-1 h-12 w-1/2 bg-secondary-500 font-inter text-sm font-medium uppercase text-brand-500 hover:bg-secondary-900 hover:text-secondary-500 data-[state=open]:bg-secondary-900 data-[state=open]:text-secondary-500 sm:hidden">
+              Geostory
+            </PopoverTrigger>
+            <PopoverContent
+              sideOffset={0}
+              side="top"
+              className="w-screen rounded-none border-none px-0 py-0"
+            >
+              <PopoverClose className="absolute left-0 top-0 block h-12 w-[60px] -translate-y-full border-none bg-brand-500 focus:text-secondary-500">
+                <ChevronDown className="mx-auto h-6 w-6 text-secondary-500" />
+              </PopoverClose>
+              <ScrollArea className="h-full max-h-[60vh] w-full" type="auto">
+                <div className="w-full space-y-1 sm:space-y-6">
+                  <div className="space-y-6">
+                    <div className="divide-y divide-secondary-900 ">
+                      {geostoryData?.monitors?.[0].id && (
+                        <Link
+                          href={`/map/${geostoryData.monitors[0].id}/geostories`}
+                          className="sticky top-0 z-10 block space-x-3 bg-brand-500 p-6 pb-2 font-bold"
+                          data-testid="back-to-monitor"
+                          style={{ color: geostoryData.color }}
+                        >
+                          <HiArrowLeft className="inline-block h-6 w-6" />
+                          <span data-testid="monitor-title-back-btn">
+                            Back to {geostoryData.monitors[0].title}.
+                          </span>
+                        </Link>
+                      )}
+                      {isGeostoryLoading && <Loading />}
+                      {geostoryData && !isGeostoryLoading && (
+                        <GeostoryHeader {...geostoryData} color={geostoryData.color} />
+                      )}
+                    </div>
+                    <div>
+                      {isLayersLoading && <Loading />}-
+                      {!!layersData?.length && !isLayersLoading && (
+                        <ul className="space-y-6" data-testid="datasets-list">
+                          {geostoryLayers.map((dataset) => (
+                            <li key={dataset.layer_id}>
+                              <DatasetCard
+                                {...dataset}
+                                type="geostory"
+                                id={dataset.layer_id}
+                                isGeostory
+                              />
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* {children} */}
-            </ScrollArea>
-          </PopoverContent>
-        </Popover>
-      </section>
-    </div>
+                {/* {children} */}
+              </ScrollArea>
+            </PopoverContent>
+          </Popover>
+        </div>
+      )}
+    </>
   );
 };
 
