94759e7ef674aae9ddd163f525a132af07ad3f76
María <maluenarod@gmail.com>
2024-08-22 12:31:05 +0200

    gesotories mobile

--- src/components/geostories/header/index.tsx
+++ src/components/geostories/header/index.tsx
@@ -8,12 +8,12 @@ import GeostoryDialog from '@/components/geostories/dialog';
 import { TAG_STYLE } from '@/styles/constants';
 
 const GeostoryHeader: FC<GeostoryParsed> = (data) => (
-  <div className="space-y-6 p-6">
+  <div className="space-y-6 p-6 text-secondary-500">
     <div className={TAG_STYLE} style={{ color: THEMES_COLORS[data.theme].base }}>
       geostory
     </div>
-    <h1 className="font-satoshi text-4xl font-bold">{data.title}</h1>
-    <p className="text-xl font-light">{data.description}</p>
+    <h1 className="font-satoshi text-3xl font-bold sm:text-4xl ">{data.title}</h1>
+    <p className="font-light sm:text-xl">{data.description}</p>
     <div className="flex items-center">
       <GeostoryDialog {...data} />
     </div>
