20ff4b43ecf0700cba464c7fc24de30022a9b3ed
María <maluenarod@gmail.com>
2024-08-23 13:45:49 +0200

    sheet removed from monitors

--- src/components/monitors/content/index.tsx
+++ src/components/monitors/content/index.tsx
@@ -15,6 +15,7 @@ import TabsNav from '@/components/tabs-nav';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { motion } from 'framer-motion';
 
 const MonitorContent = ({ children }: PropsWithChildren) => {
   const isMobile = useMediaQuery(mobile);
@@ -39,61 +40,66 @@ const MonitorContent = ({ children }: PropsWithChildren) => {
 
   return (
     <section className="monitors absolute bottom-0 left-0 z-[55] w-full border-t border-secondary-900 bg-brand-500 p-1 sm:bottom-auto sm:left-4 sm:top-[82px] sm:w-fit sm:border-0 sm:p-0">
-      {/* Desktop */}
-      <Sheet onOpenChange={onOpenChange} open={defaultOpen && open}>
-        <SheetTrigger
-          className={cn({
-            'hidden h-[60px] w-12 border-none bg-brand-500': true,
-            hidden: open,
-          })}
-        >
-          <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
-        </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="bottom-3 left-4 top-auto  hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)]"
-        >
-          <div className="bottom-3 left-4  hidden h-[calc(100vh-24px-70px)] w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 lg:block">
-            <ScrollArea className="h-full w-[526px] p-7.5" type="auto">
-              <div className="w-full space-y-1 sm:space-y-6">
-                <MonitorHeader />
-                <TabsNav />
-              </div>
-              {children}
-            </ScrollArea>
-          </div>
-          <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
-            <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
-          </SheetClose>
-        </SheetContent>
-      </Sheet>
-
-      {/* Tablet */}
-      <Sheet onOpenChange={onOpenChange} open={isTablet && defaultOpen && open}>
-        <SheetTrigger
-          className={cn({
-            'z-[60] hidden w-12 border-none bg-brand-500 sm:block md:hidden': true,
-            hidden: open,
-          })}
-        >
-          <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
-        </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="bottom-3 left-4 top-auto hidden w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)] lg:hidden"
-        >
-          <ScrollArea className="h-full w-[370px] p-7.5" type="auto">
-            <div className="w-full space-y-1 sm:space-y-6">
-              <MonitorHeader />
-              <TabsNav />
-            </div>
-            {children}
-          </ScrollArea>
-          <SheetClose className="absolute left-auto right-0 top-0 h-[60px] w-12 translate-x-full border-none bg-brand-500">
-            <ChevronDown className="mx-auto h-6 w-6 rotate-90 text-secondary-500" />
-          </SheetClose>
-        </SheetContent>
-      </Sheet>
+      {/* Desktop and tablet */}
+      <motion.section
+        initial={{ x: '-100%' }} // Start hidden and off-screen
+        animate={{
+          x: open ? 0 : '-100%', // Slide in and out
+        }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }} // Smooth easing
+        className="absolute bottom-0 left-0 z-[55] w-full border-t border-secondary-900 bg-brand-500 p-1 sm:bottom-auto sm:top-0 sm:w-fit sm:border-0 sm:p-0"
+      >
+        <div className="relative">
+          <button
+            type="button"
+            onClick={onOpenChange}
+            className="absolute -right-12 top-0 h-[60px] w-12 border-none bg-brand-500"
+          >
+            <motion.div
+              animate={{
+                rotate: open ? 180 : 0,
+              }}
+              transition={{ duration: 0.3 }}
+            >
+              <ChevronRight className="mx-auto h-6 w-6 text-secondary-500" />
+            </motion.div>
+          </button>
+          <motion.div
+            initial={{ opacity: 1, x: 0 }}
+            animate={{
+              opacity: open ? 1 : 0,
+              x: open ? 0 : '-100%',
+              transitionEnd: {
+                display: open ? 'block' : 'none',
+              },
+            }}
+            transition={{ duration: 0.3 }}
+            className="bottom-3 left-4 top-auto w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 sm:block sm:h-[calc(100vh-24px-70px)]"
+          >
+            <motion.div
+              initial={{ opacity: 1, x: 0 }}
+              animate={{
+                opacity: open ? 1 : 0,
+                x: open ? 0 : '-100%',
+                transitionEnd: {
+                  display: open ? 'block' : 'none',
+                },
+              }}
+              transition={{ duration: 0.3 }}
+              className="bottom-3 left-4 h-[calc(100vh-24px-70px)] w-fit max-w-fit rounded-none border-none bg-brand-500 px-0 py-0 lg:block"
+            >
+              <ScrollArea className="h-full p-7.5 md:w-[370px] lg:w-[526px]" type="auto">
+                <div className="w-full space-y-1 sm:space-y-6">
+                  <MonitorHeader />
+                  <TabsNav />
+                </div>
+                {children}
+                {children}
+              </ScrollArea>
+            </motion.div>
+          </motion.div>
+        </div>
+      </motion.section>
 
       <Popover onOpenChange={onOpenChange} open={isMobile && defaultOpen && open}>
         <PopoverTrigger className="z-[60] h-12 w-1/2 bg-secondary-500 font-inter text-sm font-medium uppercase text-brand-500 hover:bg-secondary-900 hover:text-secondary-500 data-[state=open]:bg-secondary-900 data-[state=open]:text-secondary-500 sm:hidden">
