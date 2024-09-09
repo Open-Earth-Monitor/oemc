b00953955f46eb9896db09c9484b212f9db88945
María <maluenarod@gmail.com>
2024-09-09 08:06:48 +0200

    tier 3 community link

--- src/components/main-menu/community-dropdown/index.tsx
+++ src/components/main-menu/community-dropdown/index.tsx
@@ -11,16 +11,17 @@ const CommunityDropdown = ({ isMobile }: { isMobile?: boolean }) => (
   <DropdownMenu modal={false}>
     <DropdownMenuTrigger
       className={cn({
-        'flex h-full items-center justify-center space-x-4 border-l border-r border-secondary-900 text-center font-satoshi font-bold transition-colors hover:bg-secondary-900 data-[state=open]:bg-secondary-500 data-[state=open]:text-brand-500 lg:min-w-[180px]':
+        'flex h-full items-center justify-center space-x-4 border-l border-r border-secondary-900  text-center font-satoshi font-bold transition-colors hover:bg-secondary-900 data-[state=open]:bg-secondary-500 data-[state=open]:text-brand-500 lg:min-w-[180px]':
           !isMobile,
-        'flex h-full w-full items-center justify-center bg-red-500 py-4 text-center font-satoshi font-bold text-secondary-500 transition-colors first:border-t first:border-secondary-900 hover:bg-secondary-500 hover:text-brand-500':
+        'flex h-full w-full items-center justify-center py-4 text-center font-satoshi font-bold text-secondary-500 transition-colors first:border-t first:border-secondary-900 hover:bg-secondary-500 hover:text-brand-500':
           isMobile,
       })}
+      classNameContent={cn({ 'justify-center': isMobile })}
     >
       <span>Communities</span>
     </DropdownMenuTrigger>
     <DropdownMenuContent
-      className="min-w-fit py-2 hover:text-secondary-700"
+      className="z-[1000] flex w-full min-w-fit flex-col py-2 hover:text-secondary-700"
       sideOffset={-1}
       alignOffset={-30}
       align="start"
@@ -29,9 +30,8 @@ const CommunityDropdown = ({ isMobile }: { isMobile?: boolean }) => (
         return (
           <Link
             className={cn({
-              'justify-left flex h-full items-center px-8 py-2 text-center font-satoshi font-bold transition-colors hover:bg-secondary-900 hover:text-secondary-500 lg:min-w-[180px]':
+              'justify-left flex h-full items-center px-8 py-2 text-center font-satoshi font-bold text-secondary-500 transition-colors hover:bg-secondary-900 lg:min-w-[180px]':
                 true,
-              'w-full justify-center bg-yellow-200': isMobile,
             })}
             key={props.href}
             {...props}
