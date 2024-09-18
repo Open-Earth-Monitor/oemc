bd72c9bad1667bab26759adec7aa6b2086b41713
María <maluenarod@gmail.com>
2024-09-18 17:36:52 +0200

    small fixes tablet

--- src/components/main-menu/community-dropdown/index.tsx
+++ src/components/main-menu/community-dropdown/index.tsx
@@ -11,9 +11,9 @@ const CommunityDropdown = ({ isMobile }: { isMobile?: boolean }) => (
   <DropdownMenu>
     <DropdownMenuTrigger
       className={cn({
-        'font-satoshi flex h-full items-center justify-center space-x-4 border-l border-r  border-secondary-900 text-center font-bold transition-colors hover:bg-secondary-900 data-[state=open]:bg-secondary-500 data-[state=open]:text-brand-500 lg:min-w-[180px]':
+        'flex h-full items-center justify-center space-x-4 border-l border-r border-secondary-900  text-center font-satoshi font-bold transition-colors hover:bg-secondary-900 data-[state=open]:bg-secondary-500 data-[state=open]:text-brand-500 lg:min-w-[180px]':
           !isMobile,
-        'font-satoshi flex h-full w-full items-center justify-center border-t-0 py-4 text-center font-bold text-secondary-500 transition-colors first:border-secondary-900 hover:bg-secondary-500 hover:text-brand-500':
+        'flex h-full w-full items-center justify-center border-t-0 py-4 text-center font-satoshi font-bold text-secondary-500 transition-colors first:border-secondary-900 hover:bg-secondary-500 hover:text-brand-500':
           isMobile,
       })}
       classNameContent={cn({ 'justify-center': isMobile })}
@@ -29,7 +29,7 @@ const CommunityDropdown = ({ isMobile }: { isMobile?: boolean }) => (
         return (
           <Link
             className={cn({
-              'justify-left font-satoshi flex h-full items-center whitespace-nowrap px-8 py-2 text-center font-bold text-secondary-500 transition-colors hover:bg-secondary-900 lg:min-w-[180px]':
+              'justify-left flex h-full items-center whitespace-nowrap px-8 py-2 text-left font-satoshi font-bold text-secondary-500 transition-colors hover:bg-secondary-900 lg:min-w-[180px]':
                 true,
             })}
             key={props.href}
