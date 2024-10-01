cbae3d5bc2691892214bba5f560529af0f4c0b54
María <maluenarod@gmail.com>
2024-10-01 07:50:58 +0200

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,8 @@ const satoshi = localFont({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${satoshi.variable} ${inter.className} h-full min-h-screen`}>
-      <body className="font-inter mx-auto h-full min-h-screen w-screen overflow-x-hidden bg-brand-500">
+    <html lang="en" className={`${satoshi.variable} ${inter.variable} h-full min-h-screen`}>
+      <body className="mx-auto h-full min-h-screen w-screen overflow-x-hidden bg-brand-500 font-inter">
         <div className="flex h-full flex-col">
           <main className="relative h-full flex-1">
             <Providers>
