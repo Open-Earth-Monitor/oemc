3a378f63f5a25a89ee748a3918b42f4099fc85fc
María <maluenarod@gmail.com>
2025-02-06 11:04:40 +0100

    no test

--- e2e/not-found.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('not found page', async ({ page }) => {
-  await page.goto('/map/monitorId/geostories', { waitUntil: 'load' });
-  await expect(page.getByTestId('404-error')).toHaveText('404');
-});
