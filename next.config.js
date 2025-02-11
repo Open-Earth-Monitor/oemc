7b2292c751e825cfeb4dd4d3de9b0382e721b18e
Andrés González <andres.gonzalez@vizzuality.com>
2025-02-11 12:54:16 +0100

    nextconfig: removes standalone

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@
 const nextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  // ? https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
-  output: 'standalone',
   poweredByHeader: false,
   env: {
     API_URL: process.env.API_URL,
