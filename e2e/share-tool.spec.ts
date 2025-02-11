6d3d57e28645ae2877f2aa3f015ee90ef3184d8a
María <maluenarod@gmail.com>
2025-02-11 17:40:54 +0100

    api route fixes

--- e2e/share-tool.spec.ts
+++ e2e/share-tool.spec.ts
@@ -5,7 +5,7 @@ import type { Monitor } from 'types/monitors';
 test.beforeEach(async ({ page }) => {
   await page.goto('/map', { waitUntil: 'load' });
   const monitorsResponse = await page.waitForResponse(
-    `${process.env.NEXT_PUBLIC_API_URL}monitors/`
+    `${process.env.NEXT_PUBLIC_API_URL}/monitors/`
   );
   const monitorsData = (await monitorsResponse.json()) as Monitor[];
   await page.getByTestId(`monitor-item-${monitorsData[0].id}`).click();
