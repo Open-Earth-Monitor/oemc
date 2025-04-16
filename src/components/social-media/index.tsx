66462c3c443616590be56ebbdb8abcf58226efb9
María <maluenarod@gmail.com>
2025-04-16 17:33:36 +0200

    WIP - landing redesign

--- src/components/social-media/index.tsx
+++ src/components/social-media/index.tsx
@@ -16,7 +16,7 @@ const SocialMedia = () => {
       const orderedData = orderBy(data, 'created_at', 'desc');
       const postsIds = orderedData.map((post) => post.id);
       const filteredData = orderedData.filter((post) => !postsIds.includes(post.reblog?.id));
-      return filteredData.slice(0, 4);
+      return filteredData;
     },
   });
 
@@ -41,38 +41,47 @@ const SocialMedia = () => {
       },
     },
   };
-
+  console.log(data);
   return (
-    <section className="container space-y-10 py-40 font-satoshi ">
-      <h3 className="text-4xl font-bold">Follow our latest news.</h3>
-      {isLoading && (
-        <div>
-          <Loading />
-        </div>
-      )}
-      {isFetched && (
-        <InView triggerOnce>
-          {({ ref }) => (
-            <motion.div
-              ref={ref}
-              className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-4"
-              variants={container}
-              initial="hidden"
-              animate="show"
-            >
-              {data?.map((post) => (
-                <motion.div
-                  key={post.id}
-                  variants={item}
-                  className="flex h-full w-full max-w-full flex-col"
-                >
-                  <Post post={post} />
-                </motion.div>
-              ))}
-            </motion.div>
-          )}
-        </InView>
-      )}
+    <section className="font-satoshi relative grid w-full grid-cols-12">
+      <div className="container col-span-12 m-auto flex flex-col items-center justify-center space-y-[18px] sm:space-y-10">
+        <h3 className="max-w-xs text-center  text-xl font-medium sm:max-w-sm sm:text-2xl md:max-w-xl lg:max-w-3xl">
+          <span className="text-white-500">
+            Open Earth Monitor connects people, data, and technology to drive global sustainability.
+          </span>{' '}
+          <span className="bg-[linear-gradient(131.67deg,_#1EEDBF_0%,_#75A1FF_100%)] bg-clip-text text-transparent">
+            Follow us to stay updated on the latest insights and innovations.
+          </span>
+        </h3>
+        {isLoading && (
+          <div>
+            <Loading />
+          </div>
+        )}
+        {isFetched && (
+          <InView triggerOnce>
+            {({ ref }) => (
+              <motion.div
+                ref={ref}
+                className=" gap-5 sm:grid-cols-2 xl:grid-cols-4"
+                variants={container}
+                initial="hidden"
+                animate="show"
+              >
+                {data?.map((post) => (
+                  <motion.div
+                    key={post.id}
+                    variants={item}
+                    className="flex h-full w-full max-w-full flex-col"
+                  >
+                    <Post post={post} />
+                  </motion.div>
+                ))}
+              </motion.div>
+            )}
+          </InView>
+        )}
+      </div>
     </section>
   );
 };
