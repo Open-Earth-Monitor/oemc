8a84d2cb7c61305461bcc1b563762e441946ca14
Bárbara Chaves <barbara.chaves@vizzuality.com>
2025-05-14 13:13:35 +0200

    Update mobile map controls

--- src/components/map/controls/constants.ts
+++ src/components/map/controls/constants.ts
@@ -3,7 +3,7 @@ export const CONTROL_BUTTON_STYLES = {
     'h-[34px] w-[34px] flex items-center justify-center rounded-full bg-brand-400 text-secondary-500 active:bg-brand-200 active:text-secondary-500 hover:text-alert',
   disabled: 'cursor-default opacity-50 pointer-events-none',
   mobile:
-    'h-[48px] w-[48px] flex items-center rounded-full justify-center bg-brand-400 text-secondary-500 active:bg-brand-200 active:text-secondary-500 hover:bg-secondary-500 hover:text-alert',
+    'h-[48px] w-[48px] flex items-center rounded-full justify-center bg-brand-400 text-secondary-500 active:bg-brand-200 active:text-secondary-500 hover:text-alert',
 };
 
 export const CONTROL_ICON_STYLES = {
--- src/components/map/monitor-map.tsx
+++ src/components/map/monitor-map.tsx
@@ -522,15 +522,13 @@ const MonitorMap: FC<CustomMapProps> = ({ initialViewState = DEFAULT_VIEWPORT })
           isMobile={isMobile}
           className="absolute right-0 top-[-134px]"
         />
-        {/* {!isMobile && (
-        )} */}
+
         <RControl.RZoom className="ol-zoom" key="ol-zoom" zoomOutLabel="-" zoomInLabel="+" />
 
         <div
           className={cn({
-            'absolute top-[-26px] flex w-full flex-col items-end justify-end space-y-1.5': true,
-            // 'top-12': isMobile,
-            // 'top-[108px]': !isMobile,
+            'absolute top-4 flex w-full flex-col items-end justify-end space-y-1.5 sm:top-[-26px]':
+              true,
           })}
         >
           <CompareRegionsStatistics isMobile={isMobile} onClick={handleRegionsLayer} />
