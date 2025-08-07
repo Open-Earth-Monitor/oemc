0bb8543612be99a54cc06fdb96196748f3082942
atrincas <atrincas@gmail.com>
2025-08-07 11:16:25 +0200

    show datasets on mobile

--- src/app/explore/layout.tsx
+++ src/app/explore/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 
-import MobileExploreToolbar from '@/containers/explore/toolbar/mobile';
-
+import MainMenuDesktop from '@/components/main-menu/desktop';
 import Map from '@/components/map';
 import { SidebarProvider } from '@/components/ui/sidebar';
 
@@ -14,11 +13,13 @@ export default function ExploreLayout({ children }: { children: React.ReactNode 
   return (
     <div className="relative">
       <SidebarProvider>{children}</SidebarProvider>
-      {/* <MapHeader /> */}
-      <div className="absolute left-0 top-0 h-screen w-screen overflow-hidden">
-        <Map />
-        <div className="block md:hidden">
-          <MobileExploreToolbar />
+      <div className="relative">
+        <div className="absolute right-5 top-7 z-50">
+          <MainMenuDesktop />
+        </div>
+
+        <div className="absolute left-0 top-0 h-screen w-screen overflow-hidden">
+          <Map />
         </div>
       </div>
     </div>
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -3,12 +3,18 @@
 import type { NextPage } from 'next';
 
 import DesktopExploreToolbar from '@/containers/explore/toolbar/desktop';
+import MobileExploreToolbar from '@/containers/explore/toolbar/mobile';
 
 const MapSidebarPage: NextPage = () => {
   return (
-    <div className="hidden md:block">
-      <DesktopExploreToolbar />
-    </div>
+    <>
+      <div className="hidden md:block">
+        <DesktopExploreToolbar />
+      </div>
+      <div className="block md:hidden">
+        <MobileExploreToolbar />
+      </div>
+    </>
   );
 };
 
