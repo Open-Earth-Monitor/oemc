709f630734965ee131e550684d697d08c9338aa4
María <maluenarod@gmail.com>
2025-08-13 08:53:43 +0200

    live stats mobile

--- src/app/usage-stats/page.tsx
+++ src/app/usage-stats/page.tsx
@@ -7,14 +7,14 @@ const UsageStatsPage: NextPage = () => {
   return (
     <div className="container m-auto h-screen font-satoshi font-medium">
       <Header className="z-50 mx-0 px-0" />
-      <div className="flex flex-col space-y-8 pt-56">
-        <h1 className="text-7xl ">
+      <div className="flex flex-col space-y-2 pt-12 sm:space-y-8 sm:pt-56">
+        <h1 className="text-3xl sm:text-7xl">
           Live{' '}
           <span className="bg-[linear-gradient(131.67deg,_#1EEDBF_0%,_#75A1FF_100%)] bg-clip-text text-transparent">
             Usage Statistics
           </span>
         </h1>
-        <h2 className="py-4 text-2xl">Web geographic activity</h2>
+        <h2 className="py-2 text-base sm:py-4 sm:text-2xl">Web geographic activity</h2>
       </div>
       <WebTrafficContent />
     </div>
