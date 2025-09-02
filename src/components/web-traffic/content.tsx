db098edc763aca844b41f4917907ac4fb717de88
María <maluenarod@gmail.com>
2025-09-02 16:25:26 +0200

    script

--- src/components/web-traffic/content.tsx
+++ src/components/web-traffic/content.tsx
@@ -6,17 +6,15 @@ const WebTrafficContent = () => {
     <div className="relative flex w-full grow flex-col">
       <section className="mt-12">
         <div className="mx-auto w-full max-w-4xl">
-          <a
-            href="https://clustrmaps.com/site/1c7ts"
-            id="clustrmaps-widget-v2"
-            title="ClustrMaps"
-            className="border-white/10 block h-[600px] w-full overflow-hidden rounded-xl border"
+          <script
+            type="text/javascript"
+            id="clustrmaps"
+            src="//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=e9bF-yBRaYPXvvGpxTgq-74ob4nqMoaLjIgTO-UoDyQ&co=092539"
           />
 
           <Script
             id="clustrmaps"
             src="//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=e9bF-yBRaYPXvvGpxTgq-74ob4nqMoaLjIgTO-UoDyQ&co=092539"
-            strategy="afterInteractive"
           />
         </div>
       </section>
