dbd96ec156e5988234d71bb2d25fed9214bac85c
María <maluenarod@gmail.com>
2025-09-02 12:50:36 +0200

    script

--- src/components/web-traffic/content.tsx
+++ src/components/web-traffic/content.tsx
@@ -5,14 +5,14 @@ import WebTrafficRankingContent from './list';
 const WebTrafficContent = () => {
   return (
     <div className="relative flex w-full grow flex-col">
-      <div className="relative my-8 h-96 min-h-[600px] w-full">
-        <Script
-          id="clustrmaps"
-          src="//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=e9bF-yBRaYPXvvGpxTgq-74ob4nqMoaLjIgTO-UoDyQ&co=092539"
-          strategy="afterInteractive"
-          className="absolute left-0 top-0 h-full w-full"
-        />
-      </div>
+      <Script
+        id="clustrmaps"
+        src="//cdn.clustrmaps.com/map_v2.js?cl=ffffff&amp;w=a&amp;t=tt&amp;d=e9bF-yBRaYPXvvGpxTgq-74ob4nqMoaLjIgTO-UoDyQ&amp;co=092539"
+      />
+      <Script
+        id="clustrmaps"
+        src="//clustrmaps.com/map_v2.js?d=aPewiJXDWdjZQbyH9Yg-r_o0pb5xj1e5wFof-PFRqKQ&cl=ffffff&w=a"
+      ></Script>
       <div className="flex flex-col py-20 sm:flex-row sm:justify-between">
         <WebTrafficRankingContent type="monitors" />
         <WebTrafficRankingContent type="geostories" />
