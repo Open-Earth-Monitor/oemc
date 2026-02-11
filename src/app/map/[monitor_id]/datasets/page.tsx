f6b4d08cd931b7b9cdaca0fdd625552abbbc26d4
María <maluenarod@gmail.com>
2026-02-11 16:07:15 +0100

    generate metadata fix

--- src/app/map/[monitor_id]/datasets/page.tsx
+++ src/app/map/[monitor_id]/datasets/page.tsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import type { Metadata, NextPage } from 'next';
+import type { Metadata } from 'next';
 
 import type { Monitor } from '@/types/monitors';
 
@@ -10,23 +9,27 @@ type Props = {
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // read route params
   const id = params.monitor_id;
 
-  // fetch data
-  const monitorData = await axios
-    .get<Monitor[]>(`${process.env.NEXT_PUBLIC_API_URL}/monitors/${id}`)
-    .then((response) => response.data);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/monitors/${id}`, {
+    next: { revalidate: 60 },
+  });
 
-  if (!monitorData?.length) return { title: 'Monitor not found' };
+  if (!res.ok) {
+    return { title: 'Monitor not found' };
+  }
+
+  const monitorData = (await res.json()) as Monitor[];
+
+  if (!monitorData?.length) {
+    return { title: 'Monitor not found' };
+  }
 
   return {
     title: `${monitorData[0].title} - Open Earth Monitor Cyberinfrastructure`,
   };
 }
 
-const DatasetsPage: NextPage<{ params: { monitor_id: string } }> = ({ params: { monitor_id } }) => {
-  return <DatasetPageComponent monitor_id={monitor_id} />;
-};
-
-export default DatasetsPage;
+export default function DatasetsPage({ params }: Props) {
+  return <DatasetPageComponent monitor_id={params.monitor_id} />;
+}
