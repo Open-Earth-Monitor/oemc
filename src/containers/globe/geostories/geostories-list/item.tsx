209b8143fdd3884647e58d503f90e0f9574fc7f3
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2026-03-05 12:56:41 +0100

    style(globe): polish geostories sidebar

--- src/containers/globe/geostories/geostories-list/item.tsx
+++ src/containers/globe/geostories/geostories-list/item.tsx
@@ -9,21 +9,27 @@ const GeostoryItem = (props: Geostory) => {
   const { id, title: label, theme } = props;
 
   return (
-    <Link href={`explore/geostory/${id}`} className="flex cursor-pointer items-center space-x-4">
+    <Link
+      href={`explore/geostory/${id}`}
+      className="group/item flex cursor-pointer items-start"
+    >
       <div
-        className="h-[79px] w-0.5 shrink-0 whitespace-normal"
+        className="mr-2 h-[79px] w-px shrink-0 whitespace-normal transition-[width] duration-200 ease-out group-hover/item:w-1"
         style={{ backgroundColor: CATEGORIES_COLORS[theme].base || '#FFFFFF' }}
       />
 
-      <Image
-        src={`/images/geostories/${id}.jpg`}
-        alt={label}
-        width={79}
-        height={79}
-        className="h-[79px] w-[79px] shrink-0 object-cover"
-      />
+      <div className="relative mr-4 h-[79px] w-[79px] shrink-0">
+        <Image
+          src={`/images/geostories/${id}.jpg`}
+          alt={label}
+          width={79}
+          height={79}
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 z-10 bg-black-500/30 transition-opacity duration-300 ease-out group-hover/item:opacity-0" />
+      </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-3">
         <span className="whitespace-nowrap text-xs font-medium text-white-500">{theme}</span>
         <span
           className="font-medium text-white-500"
