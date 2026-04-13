65f48ea971b201cf50fa404d8ba3a0873f0b3b65
María <maluenarod@gmail.com>
2026-04-13 08:34:52 +0200

    sidebar and globe filters

--- src/containers/globe/categories-filters/item.tsx
+++ src/containers/globe/categories-filters/item.tsx
@@ -49,8 +49,10 @@ const Filter = ({ id, label, Icon, className, theme }: ItemProps) => {
   return (
     <button
       className={cn(
-        'group/sidebar flex h-12 w-12 items-center justify-center rounded-full border bg-white-950 text-white-700 transition-all duration-500 hover:text-white-500',
-        { 'bg-custom-gradient text-black-500': isActive },
+        'group flex cursor-pointer items-center gap-2.5 rounded-full border border-white-950 bg-transparent p-1 transition-colors duration-300',
+        isActive
+          ? 'border-transparent'
+          : 'hover:border-[var(--category-color)] hover:text-white-500',
         className
       )}
       onClick={handleCategory}
