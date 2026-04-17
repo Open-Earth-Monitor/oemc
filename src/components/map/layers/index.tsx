c7f26109db960215935ea48682982ddf28d1c153
María <maluenarod@gmail.com>
2026-04-17 08:18:49 +0200

    basemap improvements

--- src/components/map/layers/index.tsx
+++ src/components/map/layers/index.tsx
@@ -402,6 +402,12 @@ const MapLayers: FC<CustomMapProps> = ({ initialViewState = DEFAULT_VIEWPORT }) 
 
       {isRegionsLayerActive && <NutsLayer />}
 
+      {basemap === 'world_imagery' && (
+        <RLayerTile
+          zIndex={99}
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Reference_Overlay/MapServer/tile/{z}/{y}/{x}"
+        />
+      )}
 
       <RLayerTile zIndex={100} url={labelUrl} />
     </>
