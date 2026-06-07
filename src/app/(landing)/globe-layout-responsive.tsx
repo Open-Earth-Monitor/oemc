c3d430aa4501d965583c2f853eecf46cf2002f7e
María <maluenarod@gmail.com>
2026-06-07 12:29:59 +0200

    fix: align globe explore link with cesium logo on bottom-left

--- src/app/(landing)/globe-layout-responsive.tsx
+++ src/app/(landing)/globe-layout-responsive.tsx
@@ -29,8 +29,11 @@ function GlobeLayoutDesktop() {
           <div className="-translate-y-6">
             <CategoriesFilters />
           </div>
-          <GlobeExploreData className="[@media(max-height:940px)]:mt-0" />
         </div>
+      </div>
+
+      <div className="pointer-events-none absolute bottom-2 left-5 z-10 md:bottom-[60px] xl:bottom-2">
+        <GlobeExploreData />
       </div>
     </div>
   );
@@ -46,7 +49,7 @@ function GlobeLayoutMobile() {
         <LiveUpdatesGlobeMobile />
       </div>
 
-      <div className="absolute left-5 z-10 md:bottom-[60px]">
+      <div className="absolute bottom-2 left-5 z-10 md:bottom-[60px] xl:bottom-2">
         <GlobeExploreData />
       </div>
     </>
