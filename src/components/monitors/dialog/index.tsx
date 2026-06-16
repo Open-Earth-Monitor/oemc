9187dd7827b4d43ec5fed3227f49260ac64afb30
María <maluenarod@gmail.com>
2026-06-16 16:25:07 +0200

    refactor: More info icon uses outline buttonVariants hover, native title over tooltip

--- src/components/monitors/dialog/index.tsx
+++ src/components/monitors/dialog/index.tsx
@@ -1,7 +1,10 @@
 import { LuInfo } from 'react-icons/lu';
 
+import cn from '@/lib/classnames';
+
 import type { Monitor } from '@/types/monitors';
 
+import { buttonVariants } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
